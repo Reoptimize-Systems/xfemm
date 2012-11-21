a71c2ae9498cc220912ff28056a14eeb92b535d3
s0237326 <s0237326@localhost>
2012-11-21 12:08:28 +0000

    corrected bug in mirrorsegments_mfemm.m related to copying node props, modifications to help in addsegments_mfemm.m, added function openprobleminfemm_mfemm.m

--- mfemm/preproc/addsegments_mfemm.m
+++ mfemm/preproc/addsegments_mfemm.m
@@ -3,8 +3,48 @@ function [FemmProblem, seginds] = addsegments_mfemm(FemmProblem, n0, n1, varargi
 %
 % Syntax
 % 
+% [FemmProblem, seginds] = addsegments_mfemm(FemmProblem, n0, n1)
 % [FemmProblem, seginds] = addsegments_mfemm(FemmProblem, n0, n1, 'Parameter', 'Value')
+% [FemmProblem, seginds] = addsegments_mfemm(FemmProblem, n0, n1, options)
 % 
+% Description
+%
+% addsegments_mfemm(FemmProblem, n0, n1) adds a segment to the FemmProblem
+% structure between nodes with the pairs of ids in n0 and n1.  n0 and n1
+% can be matrices of node ids of the same size, with segments being added
+% between each corresponding n0 and n1 id.
+%
+% addsegments_mfemm(FemmProblem, n0, n1, 'Parameter', 'Value') performs the
+% same operation, but applies further optional parameters to the segments,
+% as specified in one or more paramter-value pairs. Possible p-v pairs are:
+%
+%   'MaxSideLength'  - scalar value of the maximum length of triangle sides
+%                      along the segment. Defaults to -1, which means no
+%                      maximum length is set.
+%
+%   'Hidden'         - Scalar value determining the visibility in the
+%                      femm post-processor. If this evaluates to true the 
+%                      segment will be hidden. Defaults to false.
+%
+%   'InGroup'        - Scalar value containing the group number of segment. 
+%                      Defaults to zero.
+%
+%   'BoundaryMarker' - String containing the name of a boundary assigned to
+%                      segment. Defaults to an empty string, i.e. no
+%                      boundary property.
+% 
+% The values are applied to all segments created.
+%
+% addsegments_mfemm(FemmProblem, n0, n1, options) performs the
+% same operation, but instead of supplying the optional arguments as p-v
+% pairs they arer supplied as fields in an options structure. The fields
+% must have the same names as specified in the description of the p-v pairs
+% above.
+%
+%
+% See also: newsegments_mfemm.m
+%
+
 
 % Copyright 2012 Richard Crozier
 % 
