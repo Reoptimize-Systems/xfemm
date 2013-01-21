c726f22b6bee8be81978cd6e5d3c6564e149e1bd
s0237326 <3159692+crobarcro@users.noreply.github.com>
2013-01-21 09:00:01 +0000

    completed minimal functional version of fpproc_interface

--- mfemm/postproc/fpproc.m
+++ mfemm/postproc/fpproc.m
@@ -1,28 +1,28 @@
 % Class wrapper for the FPProc_interface C++ class, which offers
 % access to the FPProc finite element post-processing functions
 classdef fpproc < handle
-    properties (SetAccess = private, Hidden = true)
+    properties (SetAccess = private, Hidden = false)
         objectHandle; % Handle to the underlying C++ class instance
     end
     methods
-        %% Constructor - Create a new C++ class instance 
-        function this = class_interface(varargin)
+        %% Constructor - Create a new C++ class instance
+        function this = fpproc(varargin)
             this.objectHandle = fpproc_interface_mex('new', varargin{:});
         end
-        
+
         %% Destructor - Destroy the C++ class instance
         function delete(this)
             fpproc_interface_mex('delete', this.objectHandle);
         end
 
         %% Train - an example class method call
-        function varargout = train(this, varargin)
-            [varargout{1:nargout}] = fpproc_interface_mex('train', this.objectHandle, varargin{:});
+        function varargout = opendocument(this, filename)
+            [varargout{1:nargout}] = fpproc_interface_mex('opendocument', this.objectHandle, filename);
         end
 
         %% Test - another example class method call
-        function varargout = test(this, varargin)
-            [varargout{1:nargout}] = fpproc_interface_mex('test', this.objectHandle, varargin{:});
+        function varargout = getpointvalues(this, x, y)
+            [varargout{1:nargout}] = fpproc_interface_mex('getpointvals', this.objectHandle, x(:), y(:));
         end
     end
 end
--- mfemm/postproc/fpproc_interface_mex.cpp
+++ mfemm/postproc/fpproc_interface_mex.cpp
@@ -1,15 +1,44 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <map>
+#include <typeinfo>
 #include "mex.h"
 #include "class_handle.hpp"
 #include "fpproc_interface.h"
 
+using namespace std;
+
+// Value-Defintions of the different class methods available
+enum ClassMethods { evNotDefined,
+                    opendocument,
+                    getpointvals };
+
+// Map to associate the command strings with the class
+// method enum values
+std::map<std::string, ClassMethods> s_mapClassMethodStrs;
+
+void Initialize()
+{
+  s_mapClassMethodStrs["opendocument"] = opendocument;
+  s_mapClassMethodStrs["getpointvals"] = getpointvals;
+
+//  cout << "s_mapClassMethodStrs contains "
+//       << s_mapClassMethodStrs.size()
+//       << " entries." << endl;
+}
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
-{	
+{
     // Get the command string
-    char cmd[64];
-	if (nrhs < 1 || mxGetString(prhs[0], cmd, sizeof(cmd)))
-		mexErrMsgTxt("First input should be a command string less than 64 characters long.");
-        
+    char cmd[128];
+
+    Initialize();
+
+	if (nrhs < 1 || mxGetString(prhs[0], cmd, sizeof(cmd))){
+	    mexErrMsgTxt("First input should be a command string less than 128 characters long.");
+	}
+
     // New
     if (!strcmp("new", cmd)) {
         // Check parameters
@@ -19,11 +48,11 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         plhs[0] = convertPtr2Mat<FPProc_interface>(new FPProc_interface);
         return;
     }
-    
+
     // Check there is a second input, which should be the class instance handle
     if (nrhs < 2)
 		mexErrMsgTxt("Second input should be a class instance handle.");
-    
+
     // Delete
     if (!strcmp("delete", cmd)) {
         // Destroy the C++ object
@@ -33,30 +62,45 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
             mexWarnMsgTxt("Delete: Unexpected arguments ignored.");
         return;
     }
-    
+
     // Get the class instance pointer from the second input
-    FPProc_interface *FPProc_interface_instance = convertMat2Ptr<dummy>(prhs[1]);
-    
+    FPProc_interface *FPProc_interface_instance = convertMat2Ptr<FPProc_interface>(prhs[1]);
+
     // Call the various class methods
-    // Train    
-    if (!strcmp("train", cmd)) {
-        // Check parameters
-        if (nlhs < 0 || nrhs < 2)
-            mexErrMsgTxt("Train: Unexpected arguments.");
-        // Call the method
-        FPProc_interface_instance->train();
+    // Switch on the value
+    switch(s_mapClassMethodStrs[cmd])
+    {
+      case opendocument:
+        FPProc_interface_instance->opendocument(nlhs, plhs, nrhs, prhs);
         return;
-    }
-    // Test    
-    if (!strcmp("test", cmd)) {
-        // Check parameters
-        if (nlhs < 0 || nrhs < 2)
-            mexErrMsgTxt("Test: Unexpected arguments.");
-        // Call the method
-        FPProc_interface_instance->test();
+      case getpointvals:
+        FPProc_interface_instance->getpointvals(nlhs, plhs, nrhs, prhs);
         return;
+      default:
+        mexErrMsgTxt("Unrecognised class command string.");
+        break;
     }
-    
+
+
+    // Train
+//    if (!strcmp("train", cmd)) {
+//        // Check parameters
+//        if (nlhs < 0 || nrhs < 2)
+//            mexErrMsgTxt("Train: Unexpected arguments.");
+//        // Call the method
+//        FPProc_interface_instance->train();
+//        return;
+//    }
+//    // Test
+//    if (!strcmp("test", cmd)) {
+//        // Check parameters
+//        if (nlhs < 0 || nrhs < 2)
+//            mexErrMsgTxt("Test: Unexpected arguments.");
+//        // Call the method
+//        FPProc_interface_instance->test();
+//        return;
+//    }
+
     // Got here, so command not recognized
     mexErrMsgTxt("Command not recognized.");
 }
