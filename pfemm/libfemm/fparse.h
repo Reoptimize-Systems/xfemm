2086200262bd023238bba6a3ae6c0e128a6b5dc7
s0237326 <s0237326@localhost>
2013-02-04 14:13:55 +0000

    cleaned up project build options and added header guard to fparse.h

--- pfemm/libfemm/fparse.h
+++ pfemm/libfemm/fparse.h
@@ -2,8 +2,13 @@
 
 using namespace std;
 
+#ifndef FPARSE_H
+#define FPARSE_H
+
 char* StripKey(char *c);
 char *ParseDbl(char *t, double *f);
 char *ParseInt(char *t, int *f);
 char *ParseString(char *t, string *s);
 std::string string_format(const std::string &fmt, ...);
+
+#endif
