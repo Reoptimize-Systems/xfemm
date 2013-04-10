f8d874eb5a05da993caf3cb46e6b93902dac33fa
s0237326 <s0237326@localhost>
2013-04-10 22:11:33 +0000

    added femm namespace

--- pfemm/libfemm/fparse.h
+++ pfemm/libfemm/fparse.h
@@ -5,16 +5,24 @@ using namespace std;
 #ifndef FPARSE_H
 #define FPARSE_H
 
+namespace femm
+{
+
 // define an enum for the problem type
 enum ProblemType { PLANAR, AXISYMMETRIC };
 
 // define an enum for the coordinate system type of a problem
 enum CoordsType { CART, POLAR };
 
+// declare some functions used to parse files
 char* StripKey(char *c);
 char *ParseDbl(char *t, double *f);
 char *ParseInt(char *t, int *f);
 char *ParseString(char *t, string *s);
 std::string string_format(const std::string &fmt, ...);
 
+// declare a default warning message function
+void PrintWarningMsg(const char* message);
+
+}
 #endif
