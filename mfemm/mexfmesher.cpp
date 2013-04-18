6a7818657517c222cd6d6f9b8e3f213e8cc45a4c
s0237326 <3159692+crobarcro@users.noreply.github.com>
2013-04-18 10:47:52 +0000

    now throw error based on output status of meshing code

--- mfemm/mexfmesher.cpp
+++ mfemm/mexfmesher.cpp
@@ -66,7 +66,7 @@ int nrhs, const mxArray *prhs[])
        the string */
     std::string FilePath(buf);
 
-    /* When finished using the string, deallocate it. */
+    /* When finished using the char array, deallocate it. */
     mxFree(buf);
 
     /*  call the FMesher subroutines */
@@ -108,121 +108,120 @@ int nrhs, const mxArray *prhs[])
 
     switch (tristatus)
     {
-        case 0
-            plhs[0] = mxCreateDoubleScalar(0.0);
-            return tristatus;
-        case -1
-            plhs[0] = mxCreateDoubleScalar((double)tristatus);
-            return -2;
-        case TRIERR_OUT_OF_MEM
-            plhs[0] = mxCreateDoubleScalar((double)tristatus);
+        case 0:
+            break;
+        case -1:
+            break;
+        case TRIERR_OUT_OF_MEM:
             mexErrMsgTxt("OUT OF MEM");
-            return tristatus;
-        case TRIERR_INTERNAL_ERR
+            break;
+        case TRIERR_INTERNAL_ERR:
             mexErrMsgTxt("INTERNAL ERR");
-            return tristatus;
-        case TRIERR_ZERO_MAX_AREA
+            break;
+        case TRIERR_ZERO_MAX_AREA:
             mexErrMsgTxt("ZERO MAX AREA");
-            return tristatus;
-        case TRIERR_I_SWITCH_WITH_REFINE
+            break;
+        case TRIERR_I_SWITCH_WITH_REFINE:
             mexErrMsgTxt("I SWITCH WITH REFINE");
-            return tristatus;
-        case TRIERR_INPUT_VERTICES_IDENTICAL
+            break;
+        case TRIERR_INPUT_VERTICES_IDENTICAL:
             mexErrMsgTxt("INPUT VERTICES IDENTICAL");
-            return tristatus;
-        case TRIERR_NOT_ENOUGH_VERTICES
+            break;
+        case TRIERR_NOT_ENOUGH_VERTICES:
             mexErrMsgTxt("NOT ENOUGH VERTICES");
-            return tristatus;
-        case TRIERR_CANNOT_ACCESS_ELEFILE
+            break;
+        case TRIERR_CANNOT_ACCESS_ELEFILE:
             mexErrMsgTxt("CANNOT ACCESS ELEFILE");
-            return tristatus;
-        case TRIERR_CANNOT_ACCESS_AREAFILE
+            break;
+        case TRIERR_CANNOT_ACCESS_AREAFILE:
             mexErrMsgTxt("CANNOT ACCESS AREAFILE");
-            return tristatus;
-        case TRIERR_ELEFILE_AND_AREAFILE_DIFF_N_TRIANGLES
+            break;
+        case TRIERR_ELEFILE_AND_AREAFILE_DIFF_N_TRIANGLES:
             mexErrMsgTxt("ELEFILE AND AREAFILE DIFF N TRIANGLES");
-            return tristatus;
-        case TRIERR_TRI_INVALID_VERTEX_INDEX
+            break;
+        case TRIERR_TRI_INVALID_VERTEX_INDEX:
             mexErrMsgTxt("TRI INVALID VERTEX INDEX");
-            return tristatus;
-        case TRIERR_MISSING_VERTEX_INDEX
+            break;
+        case TRIERR_MISSING_VERTEX_INDEX:
             mexErrMsgTxt("MISSING VERTEX_INDEX");
-            return tristatus;
-        case TRIERR_SEGMENT_NO_END_POINTS
+            break;
+        case TRIERR_SEGMENT_NO_END_POINTS:
             mexErrMsgTxt("SEGMENT NO END POINTS");
-            return tristatus;
-        case TRIERR_SEGMENT_NO_SECOND_END_POINT
+            break;
+        case TRIERR_SEGMENT_NO_SECOND_END_POINT:
             mexErrMsgTxt("SEGMENT NO SECOND END POINT");
-            return tristatus;
-        case TRIERR_SEG_INVALID_VERTEX_INDEX
+            break;
+        case TRIERR_SEG_INVALID_VERTEX_INDEX:
             mexErrMsgTxt("SEG INVALID VERTEX INDEX");
-            return tristatus;
-        case TRIERR_SPLIT_SEG_TOO_SMALL
+            break;
+        case TRIERR_SPLIT_SEG_TOO_SMALL:
             mexErrMsgTxt("SPLIT SEG TOO SMALL");
-            return tristatus;
-        case TRIERR_UNEXPECTED_EOF
+            break;
+        case TRIERR_UNEXPECTED_EOF:
             mexErrMsgTxt("UNEXPECTED EOF 16");
-            return tristatus;
-        case TRIERR_CANNOT_ACCESS_POLYFILE
+            break;
+        case TRIERR_CANNOT_ACCESS_POLYFILE:
             mexErrMsgTxt("CANNOT ACCESS POLYFILE");
-            return tristatus;
-        case TRIERR_CANNOT_ACCESS_NODEFILE
+            break;
+        case TRIERR_CANNOT_ACCESS_NODEFILE:
             mexErrMsgTxt("CANNOT ACCESS NODEFILE");
-            return tristatus;
-        case TRIERR_MUST_HAVE_THREE_VERTICES
+            break;
+        case TRIERR_MUST_HAVE_THREE_VERTICES:
             mexErrMsgTxt("MUST HAVE THREE VERTICES");
-            return tristatus;
-        case TRIERR_TOO_MANY_MESH_DIMENSIONS
+            break;
+        case TRIERR_TOO_MANY_MESH_DIMENSIONS:
             mexErrMsgTxt("TOO MANY MESH DIMENSIONS");
-            return tristatus;
-        case TRIERR_VERTEX_NO_X
+            break;
+        case TRIERR_VERTEX_NO_X:
             mexErrMsgTxt("VERTEX NO X");
-            return tristatus;
-        case TRIERR_VERTEX_NO_Y
+            break;
+        case TRIERR_VERTEX_NO_Y:
             mexErrMsgTxt("VERTEX NO Y");
-            return tristatus;
-        case TRIERR_HOLE_NO_X
+            break;
+        case TRIERR_HOLE_NO_X:
             mexErrMsgTxt("HOLE NO X");
-            return tristatus;
-        case TRIERR_HOLE_NO_Y
+            break;
+        case TRIERR_HOLE_NO_Y:
             mexErrMsgTxt("HOLE NO Y");
-            return tristatus;
-        case TRIERR_REGION_NO_X
+            break;
+        case TRIERR_REGION_NO_X:
             mexErrMsgTxt("REGION NO Y");
-            return tristatus;
-        case TRIERR_REGION_NO_Y
+            break;
+        case TRIERR_REGION_NO_Y:
             mexErrMsgTxt("REGION NO Y");
-            return tristatus;
-        case TRIERR_REGION_NO_ATTRIBUTE_OR_AREA_CON
+            break;
+        case TRIERR_REGION_NO_ATTRIBUTE_OR_AREA_CON:
             mexErrMsgTxt("REGION NO ATTRIBUTE OR AREA CON");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_NODEFILE
+            break;
+        case TRIERR_CANNOT_CREATE_NODEFILE:
             mexErrMsgTxt("CANNOT CREATE NODEFILE");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_ELEFILE
+            break;
+        case TRIERR_CANNOT_CREATE_ELEFILE:
             mexErrMsgTxt("CANNOT CREATE ELEFILE");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_POLYFILE
+            break;
+        case TRIERR_CANNOT_CREATE_POLYFILE:
             mexErrMsgTxt("CANNOT CREATE POLYFILE");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_EDGEFILE
+            break;
+        case TRIERR_CANNOT_CREATE_EDGEFILE:
             mexErrMsgTxt("CANNOT CREATE EDGEFILE");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_VNODEFILE
+            break;
+        case TRIERR_CANNOT_CREATE_VNODEFILE:
             mexErrMsgTxt("CANNOT CREATE VNODEFILE");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_VEDGEFILE
+            break;
+        case TRIERR_CANNOT_CREATE_VEDGEFILE:
             mexErrMsgTxt("CANNOT CREATE VEDGEFILE");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_NEIGHBOURFILE
+            break;
+        case TRIERR_CANNOT_CREATE_NEIGHBOURFILE:
             mexErrMsgTxt("CANNOT CREATE NEIGHBOURFILE");
-            return tristatus;
-        case TRIERR_CANNOT_CREATE_OFFFILE
+            break;
+        case TRIERR_CANNOT_CREATE_OFFFILE:
             mexErrMsgTxt("CANNOT CREATE OFFFILE");
-            return tristatus;
-        default
+            break;
+        default:
             mexErrMsgTxt("Unexpected error code");
-            return -1;
+            break;
     }
+    
+    plhs[0] = mxCreateDoubleScalar((double)tristatus);
 
 }
