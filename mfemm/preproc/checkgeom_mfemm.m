14fcdbb343e6856261e20154fa1e17b20e338e1c
crobarcro <3159692+crobarcro@users.noreply.github.com>
2013-11-06 13:55:17 +0000

    lineSegmentIntersect.m: added checkgeom_mfemm.m: added getsegnodecoords_mfemm.m: added metres2length_mfemm.m: added

---
+++ mfemm/preproc/checkgeom_mfemm.m
@@ -0,0 +1,257 @@
+function varargout = checkgeom_mfemm(FemmProblem, tol, dodisplay)
+% checks a FemmProblem for comon problems such as nodes on top of each
+% other, labels on top of nodes etc.
+%
+% Syntax
+%
+% problems = checkgeom_mfemm(FemmProblem)
+% problems = checkgeom_mfemm(..., tol)
+% problems = checkgeom_mfemm(..., dodisplay)
+% checkgeom_mfemm(...)
+%
+% Description
+%
+% checkgeom_mfemm tests for various problems due to geometric entities
+% being too close together, within a given tolerance.
+% checkgeom_mfemm(FemmProblem) tests for the following problems:
+%
+%  * nodes too close to one another
+%  * nodes too close to labels
+%  * nodes too close to segments of which they are not a vertex
+%  * labels too close to one another
+%  * labels too close to segments
+%  * intersecting/overlapping segments
+%
+% By default checkgeom_mfemm uses a tolerance of 1e-5 m, this can be
+% changed by supplying the 'tol' argument with an alternative tolerance. If
+% this is supplied and is empty the default tolerance is used. 
+%
+% If no output arguments are supplied, checkgeom_mfemm prints the results
+% of the tests on the command line. Otherwise the results are silently
+% returned in a structure with the following fields:
+%
+% nodes: [n x 3 matrix], first two columns are the indices of the
+%   overlapping nodes, third is the distance between them.
+%
+% labels: [n x 3 matrix], first columns are the indices of the
+%   overlapping labels, third is the distance between them.
+%
+% labelsandnodes: [n x 3 matrix], first column is the indices of the
+%   overlapping labels, second is the indices of the nodes, third is the
+%   distance between them.
+%
+% intersectingsegments: [n x 1 matrix], first two columns are the indices
+%   of the overlapping/intersecting segments, third is the distance between them.
+%
+% labnearsegs: [n x 3 matrix], first two columns are the indices of the
+%   overlapping nodes, third is the distance between them.
+%
+% nodesnearsegs: [n x 3 matrix], first two columns are the indices of the
+%   overlapping nodes, third is the distance between them.
+%
+% Display of the results at the command line can be forced by supplying
+% dodisplay and setting its value to true.
+%
+% 
+
+    if nargin < 2 || isempty(tol)
+        tol = metres2length_mfemm(1e-5, FemmProblem.ProbInfo.LengthUnits);
+        
+        tol = tol + eps(tol);
+    end
+    
+    if nargin < 3
+        dodisplay = false;
+    end
+    
+    nodecoords = getnodecoords_mfemm(FemmProblem);
+    labelcoords = getblocklabelcoords_mfemm(FemmProblem);
+    segcoords = getsegnodecoords_mfemm(FemmProblem);
+    
+    % check for nodes within a given tolerance of each other
+    
+    % find any distances between all nodes less than the specified
+    % tolerance
+    d = ipdm(nodecoords, 'Subset', 'Maximum', 'Limit', tol, 'Result', 'Structure');
+
+    problems.nodes = [d.rowindex, d.columnindex, d.distance];
+    % strip the self referencing indices
+    problems.nodes = problems.nodes(problems.nodes(:,1) ~= problems.nodes(:,2), :);
+    
+    % check for labels within a given tolerance of each other
+    d = ipdm(labelcoords, 'Subset', 'Maximum', 'Limit', tol, 'Result', 'Structure');
+
+    problems.labels = [d.rowindex, d.columnindex, d.distance];
+    % strip the self referencing indices
+    problems.labels = problems.labels(problems.labels(:,1) ~= problems.labels(:,2), :);
+    
+    % check for labels overlapping nodes
+    d = ipdm(labelcoords, nodecoords, 'Subset', 'Maximum', 'Limit', tol, 'Result', 'Structure');
+
+    problems.labelsandnodes = [d.rowindex, d.columnindex, d.distance];
+    
+    % check for intersecting segments
+    problems.intersectingsegments = getintersectingsegments(FemmProblem, segcoords);
+    
+    % check for intersecting arc segments
+    
+    % TODO
+    
+    % check for labels near segments
+    problems.labnearsegs = labelsnearsegments(labelcoords, segcoords, tol);
+    
+    % check for labels near arc segments
+    
+    % TODO
+  
+    % check for unconnected nodes near segments
+    problems.nodesnearsegs = unconnnodesnearsegments(FemmProblem, nodecoords, segcoords, tol);
+    
+    % check for unconnected nodes near arc segments
+    
+    % TODO
+    
+    if nargout < 1
+        displayproblems(problems, tol);
+    else
+        varargout{1} = problems;
+        if dodisplay
+            displayproblems(problems, tol);
+        end
+    end
+    
+end
+
+function labnearsegs = labelsnearsegments(labelcoords, segcoords, tol)
+% checks for labels near the segments
+
+    labnearsegs = [];
+    
+    for ind = 1:size(labelcoords, 1)
+        for iind = 1:size(segcoords, 1)
+
+            d = point_to_line_2D(labelcoords(ind,:)', segcoords(iind,1:2)', segcoords(iind,3:4)');
+
+            if d <= tol
+                % store                      label   seg    distance
+                labnearsegs = [ labnearsegs; ind-1, iind-1, d ];
+            end
+
+        end
+    end
+
+end
+
+function nodesnearsegs = unconnnodesnearsegments(FemmProblem, nodecoords, segcoords, tol)
+% checks for node coords near segments of which the node is not a part
+
+    nodesnearsegs = [];
+    
+    for ind = 1:size(nodecoords, 1)
+        for iind = 1:size(segcoords, 1)
+            if (FemmProblem.Segments(iind).n0 ~= ind-1) && (FemmProblem.Segments(iind).n1 ~= ind-1)
+                % if node is not connected to the segment
+                d = point_to_line_2D(nodecoords(ind,:)', segcoords(iind,1:2)', segcoords(iind,3:4)');
+
+                if d <= tol
+                    % store                          node    seg  distance
+                    nodesnearsegs = [ nodesnearsegs; ind-1, iind-1, d ];
+                end
+            end
+        end
+    end
+
+end
+
+function intersectingsegments = getintersectingsegments(FemmProblem, segcoords)
+% finds segments which intersect or are coincident
+%
+    
+    out = lineSegmentIntersect(segcoords,segcoords);
+    
+    [intersectingsegments,intersectingsegments(:,2)] = find(out.intAdjacencyMatrix);
+    
+    intersectingsegments = intersectingsegments - 1;
+    
+    intersectingsegments(:,3) = out.coincAdjacencyMatrix(out.intAdjacencyMatrix);
+    
+    % remove and segments which are connected to the same node at either
+    % end as they will intersect at this point
+    removeentry = [];
+    for ind = 1:size(intersectingsegments, 1)
+        
+        if any(FemmProblem.Segments(intersectingsegments(ind,1)+1).n0 ...
+                == [FemmProblem.Segments(intersectingsegments(ind,2)+1).n0, ...
+                    FemmProblem.Segments(intersectingsegments(ind,2)+1).n1] ) ...
+           || any(FemmProblem.Segments(intersectingsegments(ind,1)+1).n1 ...
+                == [FemmProblem.Segments(intersectingsegments(ind,2)+1).n0, ...
+                    FemmProblem.Segments(intersectingsegments(ind,2)+1).n1] ) ...
+                    
+            removeentry = [ removeentry, ind ];
+        end
+                    
+    end
+    
+    % remove the flagged intersections
+    intersectingsegments(removeentry, :) = [];
+
+end
+
+function displayproblems(problems, tol)
+% displays the problems found on the command line
+
+    if isempty(problems.nodes)
+        fprintf(1, 'No problems were identified with overlapping nodes using tolerance %g\n', tol);
+    else
+        fprintf(1, '%d overlapping nodes were identified using tolerance %g\n', size(problems.nodes,1), tol);
+        for ind = 1:size(problems.nodes,1)
+            fprintf(1, 'nodes %d and %d, distance: %g\n', problems.nodes(ind,1), problems.nodes(ind,2), problems.nodes(ind,3));
+        end
+    end
+    
+    if isempty(problems.labels)
+        fprintf(1, 'No problems were identified with overlapping labels using tolerance %g\n', tol);
+    else
+        fprintf(1, '%d overlapping nodes were identified using tolerance %g\n', size(problems.labels,1), tol);
+        for ind = 1:size(problems.labels,1)
+            fprintf(1, 'nodes %d and %d, distance: %g\n', problems.labels(ind,1), problems.labels(ind,2), problems.labels(ind,3));
+        end
+    end
+    
+    if isempty(problems.labelsandnodes)
+        fprintf(1, 'No problems were identified with overlapping nodes and labels using tolerance %g\n', tol);
+    else
+        fprintf(1, '%d overlapping nodes and labels were identified using tolerance %g\n', size(problems.labelsandnodes,1), tol);
+        for ind = 1:size(problems.labelsandnodes,1)
+            fprintf(1, 'label %d and node %d, distance: %g\n', problems.labelsandnodes(ind,1), problems.labelsandnodes(ind,2), problems.labelsandnodes(ind,3));
+        end
+    end
+    
+    if isempty(problems.labnearsegs)
+        fprintf(1, 'No problems were identified with labels too near segments using tolerance %g\n', tol);
+    else
+        fprintf(1, '%d labels near segments identified using tolerance %g\n', size(problems.labnearsegs,1), tol);
+        for ind = 1:size(problems.labnearsegs,1)
+            fprintf(1, 'label %d and segment %d, smallest distance: %g\n', problems.labnearsegs(ind,1), problems.labnearsegs(ind,2), problems.labnearsegs(ind,3));
+        end
+    end
+    
+    if isempty(problems.nodesnearsegs)
+        fprintf(1, 'No problems were identified with nodes too near unconnected segments using tolerance %g\n', tol);
+    else
+        fprintf(1, '%d nodes near unconnected segments identified using tolerance %g\n', size(problems.nodesnearsegs,1), tol);
+        for ind = 1:size(problems.nodesnearsegs,1)
+            fprintf(1, 'node %d and segment %d, smallest distance: %g\n', problems.nodesnearsegs(ind,1), problems.nodesnearsegs(ind,2), problems.nodesnearsegs(ind,3));
+        end
+    end
+    
+    if isempty(problems.intersectingsegments)
+        fprintf(1, 'No problems were identified with intersecting segments\n');
+    else
+        fprintf(1, '%d intersecting segments identified\n', size(problems.nodesnearsegs,1));
+        for ind = 1:size(problems.intersectingsegments,1)
+            fprintf(1, 'segment %d and segment %d intersect\n', problems.nodesnearsegs(ind,1), problems.nodesnearsegs(ind,2));
+        end
+    end
+    
+end
---
+++ mfemm/preproc/metres2length_mfemm.m
@@ -0,0 +1,67 @@
+function conv = metres2length_mfemm(metres, units)
+% converts a length specified in metres to the desired input units
+% units used in the problem definition
+%
+% Syntax
+%
+% conv = length2metres_mfemm(length, units)
+%
+% Inputs
+%
+%   metres - the original length, or matrix of lengths to be converted from
+%     metres.
+%
+%   units - integer defining the original length units, as typically stored
+%     in the FemmProblem.Probinfo.LengthUnits field. units can have values
+%     0 through 5 where the numbers denote the units below:
+%
+%     0: inches
+%     1: millimeters
+%     2: centimeters
+%     3: meters
+%     4: mils
+%     5: micrometers
+%
+% Output
+%
+%   conv - the supplied lengths converted to the desired units
+%
+% See also: loadfemmfile.m, length2metres_mfemm.m
+%
+
+% Copyright 2012 Richard Crozier
+% 
+%    Licensed under the Apache License, Version 2.0 (the "License");
+%    you may not use this file except in compliance with the License.
+%    You may obtain a copy of the License at
+% 
+%        http:%www.apache.org/licenses/LICENSE-2.0
+% 
+%    Unless required by applicable law or agreed to in writing, software
+%    distributed under the License is distributed on an "AS IS" BASIS,
+%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%    See the License for the specific language governing permissions and
+%    limitations under the License.
+
+
+    switch units
+        
+        case 0
+            scaleval = 0.0254;   % inches
+        case 1
+            scaleval = 0.001;    % millimeters
+        case 2
+            scaleval = 0.01;     % centimeters
+        case 3
+            scaleval = 1;        % meters
+        case 4
+            scaleval = 2.54e-05; % mils
+        case 5
+            scaleval = 1.e-06;   % micrometers
+        otherwise
+            error('Unrecognised unit.')
+    end
+    
+    conv = metres ./ scaleval;
+
+end
