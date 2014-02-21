7433a0aeadb00bd72dffa4b9f030b6d09c886424
crobarcro <crobarcro@localhost>
2014-02-21 11:51:13 +0000

    femm_manual_tutorial_example.m: made suitible for publishing

--- mfemm/examples/femm_manual_tutorial_example.m
+++ mfemm/examples/femm_manual_tutorial_example.m
@@ -1,4 +1,4 @@
-% femm_manual_tutorial_example.m
+%% femm_manual_tutorial_example.m
 %
 %
 % This script recreates the Magnetostatics example provided in the FEMM 4.2
@@ -55,7 +55,7 @@
 %
 % 
 
-% Create a new mfemm problem structure
+%% Create a new mfemm problem structure
 %
 % newproblem_mfemm creates a new FemmProblem structure for the type of
 % problem specifiec in the first argument. This argument is either a string
@@ -71,6 +71,8 @@ FemmProblem = newproblem_mfemm('axi', ...
                                'Frequency', 0, ...
                                'LengthUnits', 'inches');
                            
+%%
+%
 % The created FemmProblem structure contains two field, ProbInfo,
 % containing the problem type specification, and Materials. The materials
 % structure contains an array of structures containing the material
@@ -80,23 +82,29 @@ FemmProblem = newproblem_mfemm('axi', ...
 % the example for use in the model.
 disp(FemmProblem)
 
+%%
+%
 % The full problem specification 
 disp(FemmProblem.ProbInfo)
 
+%%
+%
 % The next task is to create boundaries for the solution region.
 % Fundamentally, finite element solvers mesh and find a solution over a
 % finite region of space that contains the objects of interest. We must
 % define what this region is. In this case, we will choose our solution
 % region to be a sphere with a radius of 4 inches.
-
+%
 % To create the sphere we need only two nodes which can then be linked by
 % a semi-circular arc and a straight segment to make a semicircle. 
-
+%
 % first define the node locations, which are above and below the centerline
 % of the cylindrical coil.
 outernodes = [ 0, -4; 
                0,  4 ];
           
+%%
+%
 % Now add these nodes to the FemmProblem structure using the function
 % addnodes_mfemm. addnodes_mfemm is typical in that it can return up to
 % three arguments. The first of these is the modified FemmProblem structure
@@ -107,21 +115,31 @@ outernodes = [ 0, -4;
 % to store them for later use.
 [FemmProblem, nodeinds, nodeids] = addnodes_mfemm(FemmProblem, outernodes(:,1), outernodes(:,2));
 
+%%
+%
 % Next we add a straght segment between the two nodes we just created
 [FemmProblem] = addsegments_mfemm(FemmProblem, nodeids(1), nodeids(2));
 
+%%
+%
 % And then the arc segment between the same two nodes
 [FemmProblem, rcsegind] = addarcsegments_mfemm(FemmProblem, nodeids(1), nodeids(2), 180, 'MaxSegDegrees', 2.5);
 
+%%
+%
 % Next we will draw the coil, a rectangle in cross-section. The material
 % type from the materials library '18 AWG' will be used for this. So first
 % add this to the mfemm problem
 FemmProblem = addmaterials_mfemm(FemmProblem, '18 AWG');
+%%
+%
 % we will also create a circuit and add it to the model. We will specify
 % the coil current using this circuit. We will apply a 1 Amp current in
 % this circit. 
 FemmProblem = addcircuit_mfemm(FemmProblem, 'Coil', 'TotalAmps_re', 1); 
 
+%%
+%
 % Now we could draw the coil manually by adding the nodes, then the
 % segments, and then a block label with several commands, as follows:
 %
@@ -140,6 +158,8 @@ FemmProblem = addcircuit_mfemm(FemmProblem, 'Coil', 'TotalAmps_re', 1);
 %                                   'Turns', 1000, ...
 %                                   'MaxArea', 0.1);
 
+%%
+%
 % However, a more convenient method is to use the addrectregion_mfemm
 % function which combines these steps into a single call. In this case we
 % specify the block properties as the fields of a structure rather than the
@@ -152,6 +172,8 @@ CoilBlockProps.MaxArea = 0.1;
 
 FemmProblem = addrectregion_mfemm(FemmProblem, 0.5, -1, 1, 2, CoilBlockProps);
 
+%%
+%
 % We also need a material for the region surrounding the coil, in this case
 % Air. By default, when a new FemmProblem is created, Air is added as the
 % first material, so we don't need to add it again. We can just go ahead
@@ -161,6 +183,8 @@ FemmProblem = addblocklabel_mfemm(FemmProblem, 0.5, 1.5, ...
                                   'BlockType', 'Air', ...
                                   'MaxArea', 0.1);
 
+%%
+%
 % Next we must choose a boundary condition for the outer boundary of our
 % prolem to approximate the reality of the problem which is infinite in
 % extent.
@@ -190,16 +214,22 @@ mu_0 = 4 * pi *1e-7;
                                     'c0', 1/(mu_0*R), ...
                                     'c1', 0);
 
+%%
+%
 % We then need to assign this boundary condition to the arc segment. This
 % is achieved by changing the 'BoundaryMarker' of the appropriate arc
 % segment to the same name as our new boundary.
 %
 FemmProblem.ArcSegments(rcsegind).BoundaryMarker = boundname;
 
+%%
+%
 % Finally we can take a look at the problem using Matlab's plotting
 % commands
 plotfemmproblem(FemmProblem);
 
+%%
+%
 % However, this isn't very well developend yet. If the original FEMM is
 % installed, and it's m-file interface present on the Matlab search path,
 % we can open the file in FEMM as well.
@@ -211,7 +241,7 @@ else
 end
 
 %% Solve the Problem
-
+%
 % Now that the problem is defined, we want to go ahead and solve it. The
 % first step in doing this is to create a mesh. To do this we call the
 % fmesher function. Doing this will require that you have set up (i.e.
@@ -219,10 +249,14 @@ end
 % won't work/exist.
 %
 
+%%
+%
 % Create a .fem file describing the problem on disk
 filename = 'mag_tutorial.fem';
 writefemmfile(filename, FemmProblem);
 
+%%
+%
 % mesh the problem using fmesher. We could have called fmesher with the
 % FemmProblem directly, i.e.
 %
@@ -237,6 +271,8 @@ writefemmfile(filename, FemmProblem);
 % writefemmfile to generate the file.
 filename = fmesher(filename);
 
+%%
+%
 % fmesher creates a number of mesh files based on the file name with
 % different extensions, e.g. .poly, .node etc. Armed with this, we can now
 % solve the finite-element problem. This is achieved by calling fsolver.
@@ -248,7 +284,7 @@ ansfile = fsolver(filename);
 
 
 %% Extracting Results
-
+%
 % Having solved te problem, it is generally desired that we do some
 % post-processing on the results. The file which is produced is perfectly
 % compatible with FEMM, and if it is installed, we can open the file and
@@ -258,7 +294,7 @@ ansfile = fsolver(filename);
 % else
 %     fprintf(1, 'Looks like femm isn''t installed, or at least its m-files aren''t on the path.\n');
 % end
-
+%
 % However, some post-processing methods are also provided with mfemm. The
 % main method is a class based method for loading and manipulating the
 % data. The class is called fpproc, and also requires some compilation
@@ -267,9 +303,13 @@ ansfile = fsolver(filename);
 % So to manipulate the output, we first create an fpproc class
 myfpproc = fpproc();
 
+%%
+%
 % and then load the results file using the opendocument method
 myfpproc.opendocument(ansfile);
 
+%%
+%
 % once loaded fpproc provides a number of post-processing functions, which
 % are really calls to the C++ versions supplied in the original FEMM code.
 % You can view a list of all the avaialable methods using the methods
@@ -278,19 +318,28 @@ if ~isoctave
     methods(myfpproc);
 end
 
+%%
+%
 % for example, extract the values from the solution at the point (0,0)
 pvals = myfpproc.getpointvalues(0,0)
 
+%%
+%
 % we can also get multiple points, the results of which are returned in a
 % matrix with each point's values in its own column
 manypvals = myfpproc.getpointvalues([0,0.1,0.2,0.3,0.4,0.5],[0,0,0,0,0,0])
 
+%%
+%
 % alternatively, select the coil and integrate it's area. The first two
 % arguments are the location of a point inside the block area to be
 % selected, and the (optional) third argument determines whether any
 % existing selections should be cleared. If the third argument is not
 % supplied, it defaults to false, keeping existing selections.
 myfpproc.selectblock(1, 0, true);
+
+%%
+%
 % The area integral is selected by caling fpproc.blockintegral with the
 % number 5
 coilarea = myfpproc.blockintegral(5)
