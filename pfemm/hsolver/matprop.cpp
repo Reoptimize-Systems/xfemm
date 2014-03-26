7a0751cb5d0cce6ebe0977e12baf0580ce5bad47
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-03-26 22:07:33 +0000

    added missing file matprop.cpp

---
+++ pfemm/hsolver/matprop.cpp
@@ -0,0 +1,55 @@
+
+#include "hmesh.h"
+
+CMaterialProp::CMaterialProp()
+{
+    Kx=Ky=1;
+    Kt=0;
+    qv=0;
+}
+
+CMaterialProp::~CMaterialProp()
+{
+
+}
+
+CMaterialProp::CMaterialProp( const CMaterialProp & other)
+{
+    Kx = other.Kx;
+    Ky = other.Kx;
+	Kt = other.Kt;  // volumetric heat capacity
+	qv = other.qv;  // volume heat generation
+
+	// properties for nonlinear conductivity
+	npts = other.npts;			// number of points in the nonlinear conductivity curve
+
+	for (int i = 0; i < npts; i++)
+	{
+	    // copy the thermal conductivity data points.
+        Kn[i] = other.Kn[i];
+	}
+}
+
+CComplex CMaterialProp::GetK(double t)
+{
+	int i,j;
+
+	// Kx returned as real part;
+	// Ky returned as imag part
+
+	if (npts==0) return (Kx+I*Ky);
+	if (npts==1) return (Im(Kn[0])*(1+I));
+	if (t<=Re(Kn[0])) return (Im(Kn[0])*(1+I));
+	if (t>=Re(Kn[npts-1])) return (Im(Kn[npts-1])*(1+I));
+
+	for(i=0,j=1;j<npts;i++,j++)
+	{
+		if((t>=Re(Kn[i])) && (t<=Re(Kn[j])))
+		{
+			return (1+I)*(Im(Kn[i])+Im(Kn[j]-Kn[i])*Re(t-Kn[i])/Re(Kn[j]-Kn[i]));
+		}
+	}
+
+	return (Kx+I*Ky);
+}
+
