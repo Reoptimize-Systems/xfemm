a5cc83a35bab5b4036c0f89806256079bebd3cdf
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-03-27 14:18:00 +0000

    mfemm_setup.m: automated code::blocks windows build

--- mfemm/mfemm_setup.m
+++ mfemm/mfemm_setup.m
@@ -1,49 +1,76 @@
-function mfemm_setup(forceallcompile)
+function varargout = mfemm_setup(forceallcompile)
 % mfemm_setup
 %
 % A large proportion of mfemm's functionality depends on interfaces to C++
 % code. This code must be compiled into mex functions to be accessibly from
 % the matlab or octave command line. Compiled mex files for your platform
-% may be provided for your with this package, in which case mfemm_setup
-% will skip the compilation step (unless you call it as mfemm_setup(true)
-% to force recompilation). 
+% may be provided for your convenience with this package, in which case
+% mfemm_setup will skip the compilation step (unless you call it as
+% mfemm_setup(true) to force recompilation).
 %
 % If you do need/want to create the mex files, before running this script
-% you must first compile some library files for use by the mex compiler. 
+% it must first compile some library files for use by the mex compiler.
+% What is required for this depends on your system type.
 %
-% Linux: On linux type systems the process is automated, and mfemm_setup.m
-% will build the libraries using the system 'make' program, you will not
-% need to do anything. If there are problems, you can try invoking the make
-% command yourself, see the instructions for building the libraries using a
-% makefile below. 
+% ------------------------   Linux Systems    ----------------------------- 
 %
-% Windows: Unfortunately Windows lacks somewhat the development environment
-% of Linux systems. On windows, it is probably easiest to use the
-% Code::Blocks C++ IDE. Several project and workspace files for the free
-% Code::Blocks IDE are provided to ease this process. Instructions are
-% provided below for creating the required libraries using Code::Blocks.
+% On linux type systems the process is smoothly automated, and
+% mfemm_setup.m will build the libraries using the system 'make' program,
+% you will not need to do anything. If there are problems, you can try
+% invoking the make command yourself, see the instructions for building the
+% libraries using a makefile below.
 %
-% In both cases mfemm_setup requires requires that you ave a C++ compiler
+% -------------------------------------------------------------------------
+%
+% ------------------------- Windows Systems ------------------------------- 
+%
+% Unfortunately Windows lacks somewhat the development environment
+% of Linux systems. On Windows, it is easiest to install the Code::Blocks
+% C++ IDE and mingw compiler suite which usually comes with the
+% Code::Blocks package. You will also require the gnumex project which
+% allows you to use the mingw compiler as the Matlab mex compiler, there is
+% further information about gnumex below.
+%
+% mfemm_setup will call Code::Blocks automatically if it is installed in
+% the normal location to build the required libraries. You shouldn't need
+% to take any manual building steps, but you will probably have had to open
+% Code::Blocks at least once before running mfemm_setup doing this to
+% select the default compiler. If required, there is also information on
+% manually using Code::Blocks to build the libraries below.
+%
+% Code::Blocks can be obtained from here: www.codeblocks.org
+%
+% -------------------------------------------------------------------------
+%
+% In both cases mfemm_setup requires requires that you have a C++ compiler
 % set up for use by Matlab or Octave (Octave will not have a problem thanks
-% to its seamless integration with gcc). In Matlab you must have run
+% to its seamless integration with gcc). In Matlab you must have previously
+% run:
 %
 % mex -setup
 %
 % And selected a valid C++ compiler before attempting to run this
-% mfemm_setup function. For linux the system gcc compiler will be ideal.
-% The free windows compilers supported by Matlab vary over time. However,
+% mfemm_setup function. For Linux, or Octave on Windows, the system gcc
+% compiler will be ideal.
+%
+% For Windows, the compilers supported by Matlab vary over time. However,
 % you will need to build the mex files using the same compiler with which
 % you created the libraries earlier. Often there is a free version of
 % Microsoft Visual C++ which will work, but no project files are provided
 % for this. The gnumex project provides a way to use gcc on Windows with
-% Matlab if desired (this will work with the compiler which comes with
-% code::blocks, mingw).
+% Matlab (this will work with the compiler which comes with code::blocks,
+% mingw). gnumex can be found on sourceforge, here:
 %
+%                 http://gnumex.sourceforge.net/
 % 
-% ===   Instructions for building required libraries using Makefile     ===
 %
-% On your linux system change directory to the mfemm top level directory.
-% In this directory run the following command:
+%
+% = Instructions for building required libraries manually using Makefile =
+%
+% On your linux system, mfemm_setup will do this automaticaly without any
+% action required from you, but if you want to do it manually for some
+% reason you can do the following. First change directory to the mfemm top
+% level directory. In this directory run the following command:
 %
 % make
 %
@@ -54,17 +81,17 @@ function mfemm_setup(forceallcompile)
 %
 % Code::Blocks can be obtained from here: www.codeblocks.org
 %
-% To build the required library files, open the file mfemm_libs.workspace
-% file in the same directory as this funtion in Code::Blocks. This
-% Code::Blocks workspace contains several projects all of which must be
-% compiled before running the rest of the setup. You will need to manually
-% ensure that those projects which have a 'Library' build target have this
-% selected. The projects which have a 'Library' target are fmesher, fsolver
-% and fpproc. The other projects are always libraries and you will want to
-% build the "Release" versions of these. Once this has been done simply
-% select 'build workspace' from the build menu and Code::Blocks will build
-% all of the projects. Once this is done rerun mfemm_setup to create the
-% mex functions. 
+% If Code::Blocks has been installed in the usual directory, mfemm_setup
+% should find it and call it using command line arguments to build the
+% libraries without you having to do anything. However, if for some reason
+% this doesn't work. to build the required library files, open the file
+% mfemm_libs.workspace file in the same directory as this funtion in
+% Code::Blocks. This Code::Blocks workspace contains several projects all
+% of which must be compiled before running the rest of the setup. You will
+% want to build the "Release" versions of these. Once this has been done
+% simply select 'build workspace' from the build menu and Code::Blocks will
+% build all of the projects. Next rerun mfemm_setup to create the mex
+% functions.
 %
 % Note that you are building for Matlab will want your Matlab mex compiler
 % to be the same as your Code::Blocks compiler. The gnumex project
@@ -74,6 +101,12 @@ function mfemm_setup(forceallcompile)
 
     if nargin < 1
         forceallcompile = false;
+        
+        if nargout > 0
+            % return the subfunctions for testing/use
+            varargout {1} = { @makelibs };
+            return;
+        end
     end
 
     % add the required directories to the path
@@ -110,40 +143,30 @@ function mfemm_setup(forceallcompile)
 
         fprintf('Compiling mex functions for mfemm.\n');
         
-        if isunix
-            CC = onCleanup(@() cd(pwd));
-            cd (thisfilepath);
-            % if we are on a unixy computer we can make the necessary
-            % libraries using make
-            system(sprintf('make -f %s', fullfile(thisfilepath, 'Makefile')));
-        end
+        CC = onCleanup(@() cd(pwd));
+        cd (thisfilepath);
         
-        if exist(fullfile(fileparts(which('mfemm_setup.m')), 'pfemm', 'fmesher', 'libfmesher.a'), 'file') ...
-            && exist(fullfile(fileparts(which('mfemm_setup.m')), 'pfemm', 'fsolver', 'libfsolver.a'), 'file') ...
-            && exist(fullfile(fileparts(which('mfemm_setup.m')), 'pfemm', 'fpproc', 'libfpproc.a'), 'file')
-
+        makelibs (thisfilepath);
+        
+        if exist(fullfile(thisfilepath, 'pfemm', 'fmesher', 'libfmesher.a'), 'file') 
             fmeshersetup;
-            fsolversetup;
-            fpprocsetup;
-
         else
-
-            if ~exist(fullfile(fileparts(which('mfemm_setup.m')), 'pfemm', 'fmesher', 'libfmesher.a'), 'file') 
-                fprintf(1, 'mfemm_setup can''t find the libfemm library (libfmesher.a), have you built it?\nType help mfemm_setup for more info.\n')
-            end
-
-            if ~exist(fullfile(fileparts(which('mfemm_setup.m')), 'pfemm', 'fsolver', 'libfsolver.a'), 'file') 
-                fprintf(1, 'mfemm_setup can''t find the libfsolver library (libfsolver.a), have you built it?\nType help mfemm_setup for more info.\n')
-            end
-
-            if ~exist(fullfile(fileparts(which('mfemm_setup.m')), 'pfemm', 'fpproc', 'libfpproc.a'), 'file')
-                fprintf(1, 'mfemm_setup can''t find the libfpproc library (libfpproc.a), have you built the fpproc project as a library?\nType help mfemm_setup for more info.\n')
-            end
-
-            error('mfemm setup failed. Type help mfemm_setup for help setting up mfemm.')
-
+            % try building 
+            error('MFEMM:setup', 'mfemm_setup can''t find the libfmesher library (libfmesher.a), run ''help mfemm_setup'' for more info.\n')
         end
-        
+
+        if exist(fullfile(thisfilepath, 'pfemm', 'fsolver', 'libfsolver.a'), 'file') 
+            fsolversetup;
+        else
+            error('MFEMM:setup', 'mfemm_setup can''t find the libfsolver library (libfsolver.a), run ''help mfemm_setup'' for more info.\n')
+        end
+
+        if exist(fullfile(thisfilepath, 'pfemm', 'fpproc', 'libfpproc.a'), 'file')
+            fpprocsetup;
+        else
+            error('MFEMM:setup', 'mfemm_setup can''t find the libfpproc library (libfpproc.a), run ''help mfemm_setup'' for more info.\n')
+        end
+
     else
         
         fprintf('Not compiling mex functions as they already exist.\n');
@@ -151,6 +174,47 @@ function mfemm_setup(forceallcompile)
     end
 
 
+end
+
+function makelibs (thisfilepath)
+% build the libs required for mfemm
+
+    if isunix
+        % if we are on a unixy computer we can make the necessary
+        % libraries using make
+        system(sprintf('make -f %s', fullfile(thisfilepath, 'Makefile')));
+    else
+        % user must install code::blocks, but we can run it using the
+        % command line args here
+
+        % see if code::blocks is on the windows path
+        [status, result] = system ('codeblocks --help');
+
+        if status ~= 0
+            % look for it in the normal locations
+            if (exist ('C:\Program Files\CodeBlocks\codeblocks.exe', 'file'))
+                cbcmd = '"C:\Program Files\CodeBlocks\codeblocks.exe"';
+            else
+                cbcmd = '';
+            end
+        else
+            cbcmd = 'codeblocks';
+        end
+        
+        if isempty (cbcmd)
+            error ('MFEMM:setup', 'Not all the libraries required for mfemm are present, you are on windows, and I couldn''t find a code::blocks installation to make the libraries.');
+        end
+
+        fprintf (1, 'Attempting to build required libraries for mfemm using code::blocks ...\n');
+        
+        [status,~] = system ([cbcmd, '--target="Release" --build "', fullfile(thisfilepath, 'mfemm_libs.workspace'), '"'], '-echo');
+        
+        if status ~= 0
+            error ('MFEMM:setup', 'There was an error while attempting to build the mfemm libraries using code::blocks.')
+        end
+
+    end
+        
 end
 
 function t = isoctave()
