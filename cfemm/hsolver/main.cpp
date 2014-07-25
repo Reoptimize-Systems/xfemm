efa592d969ea7d2ca59caff7db764619644cb5d5
crobarcro <crobarcro@localhost>
2014-07-25 15:01:23 +0000

    main.cpp: fixed typo

--- cfemm/hsolver/main.cpp
+++ cfemm/hsolver/main.cpp
@@ -118,7 +118,7 @@ int main(int argc, char** argv)
 
     if (theHSolver.LoadPrev()==FALSE)
     {
-    printf("Loading previous solution\n");
+        printf("Loading previous solution\n");
     }
 
     // renumber using Cuthill-McKee
