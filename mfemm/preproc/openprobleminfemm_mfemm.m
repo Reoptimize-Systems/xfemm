ab1cca8b05cff894e1bd42932c17f7f4c798c95b
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-07-31 08:41:21 +0000

    openprobleminfemm_mfemm.m: added support for heat flow type problems

--- mfemm/preproc/openprobleminfemm_mfemm.m
+++ mfemm/preproc/openprobleminfemm_mfemm.m
@@ -1,9 +1,29 @@
 function filename = openprobleminfemm_mfemm(FemmProblem, filename)
-
-    if nargin < 2
-        filename = [tempname, '.fem'];
-    end
+% writes a FemmProblem structure to disk in the appropriate file format and 
+% opens the file in FEMM
+%
+% Syntax
+%
+%  filename = openprobleminfemm_mfemm(FemmProblem, filename)
+%
+% Input
+%
+%   FemmProblem - mfemm FemmProblem structure
+%
+%   filename - optional file name to be used for the output file_in_loadpath
+%
+% Output
+%
+%   filename - the name of the file written to disk
+%
     
+    if nargin < 2
+        if strncmpi (FemmProblem.Domain, 'm', 1)
+            filename = [tempname, '.fem'];
+        elseif strncmpi (FemmProblem.Domain, 'h', 1)
+            filename = [tempname, '.feh'];
+        end
+    end
     writefemmfile(filename, FemmProblem);
     % open femm if we have not already done so
     openfemm;
