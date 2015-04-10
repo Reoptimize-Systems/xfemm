6fd2d4dfdcdd4864a588fb8f6e10435fc927d773
crobarcro <crobarcro@localhost>
2015-04-10 13:58:10 +0000

    preproc/translatenodes_mfemm.m: updated copyright

--- mfemm/preproc/translatenodes_mfemm.m
+++ mfemm/preproc/translatenodes_mfemm.m
@@ -8,7 +8,7 @@ function [FemmProblem] = translatenodes_mfemm(FemmProblem, x, y, nodeids)
 %
 % 
 
-% Copyright 2012 Richard Crozier
+% Copyright 2012-2015 Richard Crozier
 % 
 %    Licensed under the Apache License, Version 2.0 (the "License");
 %    you may not use this file except in compliance with the License.
