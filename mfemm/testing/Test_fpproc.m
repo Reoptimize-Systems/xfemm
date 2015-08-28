b2b98fdd6931835bdbe08c55e3ab0a70a85fccd7
crobarcro <crobarcro@localhost>
2015-08-28 08:52:47 +0000

    Added test script for fpproc and reference problem for testing

---
+++ mfemm/testing/Test_fpproc.m
@@ -0,0 +1,28 @@
+%% Fpproc Unit tests
+
+
+% solve reference problem
+femprob = fullfile (getmfilepath ('Test_fpproc.m'), 'reference.fem');
+[ansfilename, femfilename] = analyse_mfemm(femprob);
+
+solution = fpproc (ansfilename);
+
+%% line integral
+
+% make a contour
+solution.clearcontour ()
+solution.addcontour (-2, 2);
+solution.addcontour (2, 2)
+solution.addcontour (2, -2)
+solution.addcontour (-2, -2)
+solution.addcontour (-2, 2);
+
+% perform some integrals
+Bn = solution.lineintegral (0)
+Ht = solution.lineintegral (1)
+contour_length = solution.lineintegral (2)
+force_from_stress_tensor = solution.lineintegral (3)
+torque_from_stress_tensor = solution.lineintegral (4)
+Bn2 = solution.lineintegral (5)
+
+
