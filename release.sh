11ee2078febd70b51583524384efe4a979da3540
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2016-03-02 16:16:45 +0000

    release.sh: use more variables to make changes easier

--- release.sh
+++ release.sh
@@ -19,24 +19,26 @@ mkdir $WORKING_COPY_DIR/release
 
 # linux 64 bit
 #mkdir $WORKING_COPY_DIR/release/xfemm_linux64
+LINUX_64_DIR="xfemm_linux64"
+LINUX_64_RELEASE_DIR="$WORKING_COPY_DIR/release/$LINUX_64_DIR"
 # export from the working directory to the release directory
-hg archive $WORKING_COPY_DIR/release/xfemm_linux64
+hg archive $LINUX_64_RELEASE_DIR
 # remove file created by mercurial
-rm $WORKING_COPY_DIR/release/xfemm_linux64/.hg_archival.txt
+rm $LINUX_64_RELEASE_DIR/.hg_archival.txt
 # remove the release script
-rm $WORKING_COPY_DIR/release/xfemm_linux64/release.sh
-rm $WORKING_COPY_DIR/release/xfemm_linux64/test_release.sh
+rm $LINUX_64_RELEASE_DIR/release.sh
+rm $LINUX_64_RELEASE_DIR/test_release.sh
 # create temp build directory
-rm -rf /tmp/xfemm_linux64
-mkdir -p /tmp/xfemm_linux64
+rm -rf /tmp/$LINUX_64_DIR
+mkdir -p /tmp/$LINUX_64_DIR
 # run cmake from temp dir and build
-cd /tmp/xfemm_linux64
-cmake -DCMAKE_BUILD_TYPE=Release $WORKING_COPY_DIR/release/xfemm_linux64/cfemm
+cd /tmp/$LINUX_64_DIR
+cmake -DCMAKE_BUILD_TYPE=Release $LINUX_64_RELEASE_DIR/cfemm
 make
 
 # tar up the result in the release directory
 cd $WORKING_COPY_DIR/release
-tar cvzf xfemm_v${VERSION}_linux64.tar.gz xfemm_linux64/
+tar cvzf xfemm_v${VERSION}_linux64.tar.gz $LINUX_64_DIR/
 
 
 # repeat the above, but cross-compiling for windows, requires MXE: http://mxe.cc/
@@ -45,23 +47,24 @@ tar cvzf xfemm_v${VERSION}_linux64.tar.gz xfemm_linux64/
 # win 32 -- i686-pc-mingw32
 #svn export $WORKING_COPY_DIR $WORKING_COPY_DIR/release/xfemm_mingw_win32 --native-eol CRLF
 #rm $WORKING_COPY_DIR/release/xfemm_mingw_win32/release.sh
-
+WIN_64_DIR="xfemm_mingw_win64"
+WIN_64_RELEASE_DIR="$WORKING_COPY_DIR/release/$WIN_64_DIR"
 # win 64 -- x86_64-w64-mingw32 MXE target
-hg archive $WORKING_COPY_DIR/release/xfemm_mingw_win64 # TODO: windows line endings?
+hg archive $WIN_64_RELEASE_DIR # TODO: windows line endings?
 # remove file created by hg
-rm $WORKING_COPY_DIR/release/xfemm_linux64/.hg_archival.txt
+rm $WIN_64_RELEASE_DIR/.hg_archival.txt
 # remove release scripts
-rm $WORKING_COPY_DIR/release/xfemm_mingw_win64/release.sh
-rm $WORKING_COPY_DIR/release/xfemm_linux64/test_release.sh
+rm $WIN_64_RELEASE_DIR/release.sh
+rm $WIN_64_RELEASE_DIR/test_release.sh
 # create temp build directory
-rm -rf /tmp/xfemm_mingw_win64
-mkdir -p /tmp/xfemm_mingw_win64
+rm -rf /tmp/$WIN_64_DIR
+mkdir -p /tmp/$WIN_64_DIR
 # run cmake from temp dir and build cfemm
-cd /tmp/xfemm_mingw_win64
+cd /tmp/$WIN_64_DIR
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DEXTRA_CMAKE_CXX_FLAGS="-static -static-libgcc -static-libstdc++" \
       -DEXTRA_CMAKE_EXE_LINKER_FLAGS="-static  -static-libgcc -static-libstdc++" \
-      -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/x86_64-w64-mingw32.static/share/cmake/mxe-conf.cmake $WORKING_COPY_DIR/release/xfemm_mingw_win64/cfemm
+      -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/x86_64-w64-mingw32.static/share/cmake/mxe-conf.cmake $WIN_64_RELEASE_DIR/cfemm
 make
 
 # copy over the windows dlls, and delete from dist
@@ -71,7 +74,7 @@ make
 
 # tar up the result in the release directory
 cd $WORKING_COPY_DIR/release
-zip -r xfemm_v${VERSION}_mingw_win64.zip xfemm_mingw_win64/
+zip -r xfemm_v${VERSION}_mingw_win64.zip $WIN_64_DIR/
 
 
 
