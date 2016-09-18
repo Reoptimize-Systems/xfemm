e4166b5f0853d47b1391f9ba2daf5bf686c24a2f
Johannes Zarl-Zierl <1199511+jzarl@users.noreply.github.com>
2016-09-18 19:55:56 +0200

    Add doxygen configuration.

---
+++ cfemm/apidoc.h
@@ -0,0 +1,7 @@
+/*!
+ * \defgroup LuaCommon Common Lua Functions
+ * \defgroup LuaMM Magnetics Lua Functions
+ * \defgroup LuaES Electrostatics Lua Functions
+ * \defgroup LuaHF Heat Flow Lua Functions
+ * \defgroup LuaCF Current Flow Lua Functions
+ */
