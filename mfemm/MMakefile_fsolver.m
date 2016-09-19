9688a41830e65838f324bab78b77a2b53c9be9ff
Richard Crozier <a@b.c>
2016-09-19 16:18:28 +0100

    Improvements to build system to fix building on windows and allow cross-build

--- mfemm/MMakefile_fsolver.m
+++ mfemm/MMakefile_fsolver.m
@@ -1,19 +1,22 @@
-function [rules,vars] = MMakefile_fsolver ()
+function [rules,vars] = MMakefile_fsolver (varargin)
 
 %     mfemmdeps.getmfilepath (mfilename);
 
-    if ispc
-        trilibraryflag = '-DCPU86';
-    else
-        trilibraryflag = '-DLINUX';
-    end
+%     if ispc
+%         trilibraryflag = '-DCPU86';
+%     else
+%         trilibraryflag = '-DLINUX';
+%     end
 
     % flags that will be passed direct to mex
-    vars.MEXFLAGS = ['${MEXFLAGS} -I"../cfemm/fsolver" -I"../cfemm/libfemm" -I"../cfemm/libfemm/liblua" ', trilibraryflag];
+%     vars.MEXFLAGS = ['${MEXFLAGS} -I"../cfemm/fsolver" -I"../cfemm/libfemm" -I"../cfemm/libfemm/liblua" ', trilibraryflag];
+    vars.MEXFLAGS = '${MEXFLAGS} -I"../cfemm/fsolver" -I"../cfemm/libfemm" -I"../cfemm/libfemm/liblua" ';
     if isunix && ~mfemmdeps.isoctave ()
         vars.OPTIMFLAGS = ['-O2'];
         vars.MEXFLAGS = [vars.MEXFLAGS, ' CXXOPTIMFLAGS="-O2 -DNDEBUG"'];
     end
+    
+    vars.LDFLAGS = '${LDFLAGS} -lstdc++';
     
     vars.OBJS = { ...
       ... % liblua
@@ -63,7 +66,7 @@ function [rules,vars] = MMakefile_fsolver ()
     %     mex $^ -output $@
     rules(1).target = {'mexfsolver.${MEX_EXT}'};
     rules(1).deps = vars.OBJS;
-    rules(1).commands = 'mex ${MEXFLAGS} $^ -output $@';
+    rules(1).commands = 'mex ${MEXFLAGS} $^ dummy.cpp -output $@';
     
     % created the following using:
     % clc
