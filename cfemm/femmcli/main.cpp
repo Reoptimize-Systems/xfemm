c7ea7f33ff7a8a2e4e225090c5c10d6d43b33710
Johannes Zarl-Zierl <1199511+jzarl@users.noreply.github.com>
2016-11-09 03:15:27 +0100

    Add fsolver support to femmcli

--- cfemm/femmcli/main.cpp
+++ cfemm/femmcli/main.cpp
@@ -17,6 +17,13 @@
 #include <iostream>
 #include <string>
 
+#define DEBUG_FEMMCLI
+#ifdef DEBUG_FEMMCLI
+#define debug std::cerr
+#else
+#define debug while(false) std::cerr
+#endif
+
 using namespace femm;
 
 enum OperationMode { NoOperation, LuaMode, SolverMode };
@@ -91,7 +98,176 @@ int execLuaFile( const std::string &inputFile, const std::string &luaInit)
  */
 int solveFile( const std::string &inputFile)
 {
-    return 0;
+    if (inputFile.size() < 3)
+    {
+        std::cerr << "Input file name too short.\n";
+        return 1;
+    }
+    auto dotIdx = inputFile.find_last_of('.');
+    std::string ending = inputFile.substr(dotIdx+1);
+    to_lower(ending);
+    if (ending == "fem")
+    {
+        FSolver theFSolver;
+        theFSolver.PathName = inputFile.substr(0,dotIdx);
+        debug << inputFile << " is a magnetics file; setting PathName to " << theFSolver.PathName << "\n";
+
+        if (theFSolver.LoadProblemFile () != true)
+        {
+            theFSolver.WarnMessage("problem loading .fem file\n");
+            return 1;
+        }
+
+        // load mesh
+        int err = theFSolver.LoadMesh();
+        if (err != 0)
+        {
+            theFSolver.WarnMessage("problem loading mesh:\n");
+
+            switch (err)
+            {
+                case ( BADEDGEFILE ):
+                    theFSolver.WarnMessage("Could not open .edge file.\n");
+                    break;
+
+                case ( BADELEMENTFILE ):
+                    theFSolver.WarnMessage("Could not open .ele file.\n");
+                    break;
+
+                case( BADFEMFILE ):
+                    theFSolver.WarnMessage("Could not open .fem file.\n");
+                    break;
+
+                case( BADNODEFILE ):
+                    theFSolver.WarnMessage("Could not open .node file.\n");
+                    break;
+
+                case( BADPBCFILE ):
+                    theFSolver.WarnMessage("Could not open .pbc file.\n");
+                    break;
+
+                case( MISSINGMATPROPS ):
+                    theFSolver.WarnMessage("Material properties have not been defined for all regions.\n");
+                    break;
+
+                default:
+                    theFSolver.WarnMessage("AN unknown error occured.\n");
+                    break;
+            }
+
+            return 2;
+        }
+
+        // renumber using Cuthill-McKee
+        std::cout << "renumbering nodes\n";
+        if (theFSolver.Cuthill() != true)
+        {
+            theFSolver.WarnMessage("problem renumbering node points");
+            return 3;
+        }
+
+        std::cout << "solving...\n";
+
+        std::cout.precision(2);
+        std::cout.width(3);
+        std::cout << "Problem Statistics:\n"
+                  << theFSolver.NumNodes << " nodes\n"
+                  << theFSolver.NumEls << " elements\n"
+                  << "Precision: " << theFSolver.Precision << "\n";
+
+
+        //    double mr = (8.*((double) Doc.NumNodes)*((double) Doc.BandWidth)) / 1.e06;
+
+        if(theFSolver.Frequency == 0)
+        {
+
+            CBigLinProb L;
+
+            L.Precision = theFSolver.Precision;
+
+            // initialize the problem, allocating the space required to solve it.
+            if (L.Create(theFSolver.NumNodes, theFSolver.BandWidth) == false)
+            {
+                theFSolver.WarnMessage("couldn't allocate enough space for matrices");
+                return 4;
+            }
+
+            // Create element matrices and solve the problem;
+            if (theFSolver.ProblemType == false)
+            {
+                if (theFSolver.Static2D(L) == false)
+                {
+                    theFSolver.WarnMessage("Couldn't solve the problem");
+                    return 5;
+                }
+                std::cout << "Static 2-D problem solved\n";
+            }
+            else
+            {
+
+                if (theFSolver.StaticAxisymmetric(L) == false)
+                {
+                    theFSolver.WarnMessage("Couldn't solve the problem");
+                    return 5;
+                }
+                std::cout << "Static axisymmetric problem solved\n";
+            }
+
+            if (theFSolver.WriteStatic2D(L) == false)
+            {
+                theFSolver.WarnMessage("couldn't write results to disk");
+                return 6;
+            }
+            std::cout << "results written to disk\n";
+        }
+        else
+        {
+            CBigComplexLinProb L;
+
+            L.Precision = theFSolver.Precision;
+
+            // initialize the problem, allocating the space required to solve it.
+
+            if (L.Create(theFSolver.NumNodes+theFSolver.NumCircProps, theFSolver.BandWidth, theFSolver.NumNodes) == false)
+            {
+                theFSolver.WarnMessage("couldn't allocate enough space for matrices");
+                return 4;
+            }
+
+            // Create element matrices and solve the problem;
+            if (theFSolver.ProblemType == false)
+            {
+                if (theFSolver.Harmonic2D(L) == false)
+                {
+                    theFSolver.WarnMessage("Couldn't solve the problem");
+                    return 5;
+                }
+                std::cout << "Harmonic 2-D problem solved\n";
+            }
+            else
+            {
+                if (theFSolver.HarmonicAxisymmetric(L) == false)
+                {
+                    theFSolver.WarnMessage("Couldn't solve the problem");
+                    return 5;
+                }
+                std::cout << "Harmonic axisymmetric problem solved\n";
+            }
+
+
+            if (theFSolver.WriteHarmonic2D(L)==false)
+            {
+                theFSolver.WarnMessage("couldn't write results to disk");
+                return 6;
+            }
+            std::cout << "results written to disk.\n";
+        }
+        return 0;
+    }
+
+    // fall-through:
+    std::cerr << "Could not determine file type!\n";
+    return 1;
 }
 
 int main(int argc, char ** argv)
