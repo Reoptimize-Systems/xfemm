4eabd78e28a4072394a71e5a63c413c3ff43acd5
Johannes Zarl-Zierl <1199511+jzarl@users.noreply.github.com>
2017-02-02 16:19:59 +0100

    implement mi_setgroup

--- cfemm/femmcli/LuaMagneticsCommands.cpp
+++ cfemm/femmcli/LuaMagneticsCommands.cpp
@@ -235,8 +235,8 @@ void femmcli::LuaMagneticsCommands::registerCommands(LuaInstance &li)
     li.addFunction("mi_setgrid", LuaInstance::luaNOP);
     li.addFunction("mo_set_grid", LuaInstance::luaNOP);
     li.addFunction("mo_setgrid", LuaInstance::luaNOP);
-    li.addFunction("mi_set_group", luaSetgroupNOP);
-    li.addFunction("mi_setgroup", luaSetgroupNOP);
+    li.addFunction("mi_set_group", luaSetGroup);
+    li.addFunction("mi_setgroup", luaSetGroup);
     li.addFunction("mi_set_node_prop", luaSetNodeProp);
     li.addFunction("mi_setnodeprop", luaSetNodeProp);
     li.addFunction("mi_set_segment_prop", luaSetSegmentProp);
@@ -3104,15 +3104,55 @@ int femmcli::LuaMagneticsCommands::luaSetEditMode(lua_State *L)
 }
 
 /**
- * @brief FIXME not implemented
+ * @brief Set the group of selected items and unselect them.
  * @param L
  * @return 0
  * \ingroup LuaMM
  * \femm42{femm/femmeLua.cpp,lua_setgroup()}
+ *
+ * \internal
+ * mi_setgroup(n) Set the group associated of the selected items to n
  */
-int femmcli::LuaMagneticsCommands::luaSetgroupNOP(lua_State *L)
+int femmcli::LuaMagneticsCommands::luaSetGroup(lua_State *L)
 {
-    lua_error(L, "Not implemented.");
+    auto luaInstance = LuaInstance::instance(L);
+    std::shared_ptr<FemmState> femmState = std::dynamic_pointer_cast<FemmState>(luaInstance->femmState());
+    std::shared_ptr<FemmProblem> doc = femmState->femmDocument;
+
+    int n=lua_gettop(L);
+    int grp;
+    if (n>0)
+        grp =(int) lua_todouble(L,1);
+    else
+        return 0;
+
+    for(auto &node: doc->nodelist)
+    {
+        if(node->IsSelected)
+            node->InGroup=grp;
+    }
+
+    for(auto &line: doc->linelist)
+    {
+        if(line->IsSelected)
+            line->InGroup=grp;
+    }
+
+    for(auto &arc: doc->arclist)
+    {
+        if(arc->IsSelected)
+            arc->InGroup=grp;
+    }
+
+    for(auto &label: doc->labellist)
+    {
+        if(label->IsSelected)
+            label->InGroup=grp;
+    }
+
+    std::shared_ptr<fmesher::FMesher> mesher = femmState->getMesher();
+    mesher->UnselectAll();
+
     return 0;
 }
 
