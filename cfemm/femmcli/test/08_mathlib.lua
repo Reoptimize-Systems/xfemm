efa3d1be34490ce9a37a7dad65290c7eb1af3c04
Johannes Zarl-Zierl <1199511+jzarl@users.noreply.github.com>
2017-06-22 22:26:54 +0200

    Add some basic tests for lua math functions.

---
+++ cfemm/femmcli/test/08_mathlib.lua
@@ -0,0 +1,13 @@
+-- 08_mathlib.lua
+-- OUTPUT:
+-- SUCCESS
+
+-- test some math functions
+assert( min( 9, max(8, 7)) == 8 )
+assert( log10(10) == 1)
+assert( cos(0) == 1 )
+assert( floor( 4.5 ) == 4 )
+assert( ceil( 4.5 ) == 5 )
+
+-- make sure pi is defined:
+assert( abs(PI - 3.14159265358979) < .00000000000001 )
