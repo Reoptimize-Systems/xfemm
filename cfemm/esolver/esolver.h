6fe743129b40f1eb02ff3378d2a278d9d851dfc2
Johannes Zarl-Zierl <1199511+jzarl@users.noreply.github.com>
2017-06-26 23:37:28 +0200

    WIP: start esolver folder
    Files are currently copies of hsolver.

---
+++ cfemm/esolver/esolver.h
@@ -0,0 +1,98 @@
+/*
+   This code is a modified version of an algorithm
+   forming part of the software program Finite
+   Element Method Magnetics (FEMM), authored by
+   David Meeker. The original software code is
+   subject to the Aladdin Free Public Licence
+   version 8, November 18, 1999. For more information
+   on FEMM see www.femm.info. This modified version
+   is not endorsed in any way by the original
+   authors of FEMM.
+
+   This software has been modified to use the C++
+   standard template libraries and remove all Microsoft (TM)
+   MFC dependent code to allow easier reuse across
+   multiple operating system platforms.
+
+   Date Modified: 2014 - 03 - 21
+   By:  Emoke Szelitzky
+        Tibor Szelitzky
+        Richard Crozier
+		  Johannes Zarl-Zierl
+   Contact:
+        szelitzkye@gmail.com
+        sztibi82@yahoo.com
+        richard.crozier@yahoo.co.uk
+		  johannes.zarl-zierl@jku.at
+
+	Contributions by Johannes Zarl-Zierl were funded by
+	Linz Center of Mechatronics GmbH (LCM)
+*/
+
+// esolver.h : interface of the ESolver class
+
+#ifndef ESOLVER_H
+#define ESOLVER_H
+
+#include "feasolver.h"
+#include "hspars.h"
+#include "CBlockLabel.h"
+#include "CBoundaryProp.h"
+#include "CCircuit.h"
+#include "CNode.h"
+#include "CMaterialProp.h"
+#include "CPointProp.h"
+
+#include <string>
+
+class ESolver : public FEASolver<
+        femm::CHPointProp
+        , femm::CHBoundaryProp
+        , femm::CHMaterialProp
+        , femm::CHConductor
+        , femm::CHBlockLabel
+        , femm::CNode
+        >
+{
+
+// Attributes
+public:
+
+	ESolver();
+	~ESolver();
+
+
+    // General problem attributes
+    double	dT; ///< \brief delta T used by esolver \verbatim[dT]\endverbatim
+
+    // mesh information
+    femm::CNode *meshnode;
+
+	// Vector containing previous solution for time-transient analysis
+	double *Tprev;
+
+
+// Operations
+public:
+
+    int LoadMesh(bool deleteFiles=true);
+    int LoadPrev();
+    bool LoadProblemFile();
+    double ChargeOnConductor(int OnConductor, CHBigLinProb &L);
+	int WriteResults(CHBigLinProb &L);
+    int AnalyzeProblem(CHBigLinProb &L);
+    void (*WarnMessage)(const char*);
+
+private:
+
+    void MsgBox(const char* message);
+    void CleanUp();
+
+    // override parent class virtual method
+    void SortNodes (int* newnum);
+
+    virtual bool handleToken(const std::string &token, std::istream &input, std::ostream &err) override;
+
+};
+
+#endif
