f0c0c4f16cd037d3c2a8e9a7b44f136476b56d0e
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2017-10-15 00:32:29 +0200

    Fix bug in *pproc.lua test cases, adjust margins in epproc to not fail.

--- cfemm/femmcli/test/femmcli_fpproc.lua
+++ cfemm/femmcli/test/femmcli_fpproc.lua
@@ -4,21 +4,21 @@
 -- SUCCESS
 showconsole()
 
-SUCCESS=1
-
 -- check variable <name>,
 -- compare <value> against <expected> value
--- if the relative difference is greater than the margin (in percent), complain and set global variable SUCCESS to false
+-- if the relative difference is greater than the margin (in percent), complain and return 1
 function check(name, value, expected, margin)
 	diff=100*(value - expected) / expected
 	if abs(diff) > margin then
-		SUCCESS=0
+		fail=1
 		result="[FAILED] "
 	else
+		fail=0
 		result="[  ok  ] "
 	end
 	print(result .. name .. ": " .. value .. " (expected: " .. expected .. ", diff: " .. diff .. "%, margin: " .. margin .. "%)")
 	--print("check(\""..name.."\", "..name..", "..value..", "..margin..")")
+	return fail
 end
 
 -- enable for additional output:
@@ -32,19 +32,20 @@ A,B1,B2,Sig,E,H1,H2,Je,Js,Mu1,Mu2,Pe,Ph = mo_getpointvalues(0.250, 0)
 
 -- check result against FEMM42 output:
 -- FIXME: error margin needs sane values
-check("A", A, 1.245741227364988e-014, 2)
-check("B1", B1, -9.855007421888915e-014, 2)
-check("B2", B2, 3.052725906923963e-014, 2)
-check("Sig", Sig, 0, 2)
-check("E", E, 4.235125240802008e-021, 3)
-check("H1", H1, -7.842365727004682e-008, 2)
-check("H2", H2, 2.429282089958189e-008, 2)
-check("Je", Je, 0, 2)
-check("Js", Js, 0, 2)
-check("Mu1", Mu1, 1, 2)
-check("Mu2", Mu2, 1, 2)
-check("Pe", Pe, 0, 2)
-check("Ph ", Ph , 0, 2)
+failed=0
+failed = failed + check("A", A, 1.245741227364988e-014, 2)
+failed = failed + check("B1", B1, -9.855007421888915e-014, 2)
+failed = failed + check("B2", B2, 3.052725906923963e-014, 2)
+failed = failed + check("Sig", Sig, 0, 2)
+failed = failed + check("E", E, 4.235125240802008e-021, 3)
+failed = failed + check("H1", H1, -7.842365727004682e-008, 2)
+failed = failed + check("H2", H2, 2.429282089958189e-008, 2)
+failed = failed + check("Je", Je, 0, 2)
+failed = failed + check("Js", Js, 0, 2)
+failed = failed + check("Mu1", Mu1, 1, 2)
+failed = failed + check("Mu2", Mu2, 1, 2)
+failed = failed + check("Pe", Pe, 0, 2)
+failed = failed + check("Ph ", Ph , 0, 2)
 
-assert(SUCCESS)
+assert(failed==0)
 write("SUCCESS\n")
