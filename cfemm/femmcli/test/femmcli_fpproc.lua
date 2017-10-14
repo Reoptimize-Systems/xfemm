d4c7f73c8873e0f75f755e322634cdf5f43180c3
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2017-10-14 23:38:24 +0200

    Switch pproc test cases in femmcli to relative differences

--- cfemm/femmcli/test/femmcli_fpproc.lua
+++ cfemm/femmcli/test/femmcli_fpproc.lua
@@ -6,15 +6,18 @@ showconsole()
 
 SUCCESS=1
 
+-- check variable <name>,
+-- compare <value> against <expected> value
+-- if the relative difference is greater than the margin (in percent), complain and set global variable SUCCESS to false
 function check(name, value, expected, margin)
-	diff=value - expected
+	diff=100*(value - expected) / expected
 	if abs(diff) > margin then
 		SUCCESS=0
 		result="[FAILED] "
 	else
 		result="[  ok  ] "
 	end
-	print(result .. name .. ": " .. value .. " (expected: " .. expected .. ", diff: " .. diff .. ", margin: " .. margin .. ")")
+	print(result .. name .. ": " .. value .. " (expected: " .. expected .. ", diff: " .. diff .. "%, margin: " .. margin .. "%)")
 	--print("check(\""..name.."\", "..name..", "..value..", "..margin..")")
 end
 
@@ -29,19 +32,19 @@ A,B1,B2,Sig,E,H1,H2,Je,Js,Mu1,Mu2,Pe,Ph = mo_getpointvalues(0.250, 0)
 
 -- check result against FEMM42 output:
 -- FIXME: error margin needs sane values
-check("A", A, 1.245741227364988e-014, 0.01)
-check("B1", B1, -9.855007421888915e-014, 0.01)
-check("B2", B2, 3.052725906923963e-014, 0.01)
-check("Sig", Sig, 0, 0.01)
-check("E", E, 4.235125240802008e-021, 0.01)
-check("H1", H1, -7.842365727004682e-008, 0.01)
-check("H2", H2, 2.429282089958189e-008, 0.01)
-check("Je", Je, 0, 0.01)
-check("Js", Js, 0, 0.01)
-check("Mu1", Mu1, 1, 0.01)
-check("Mu2", Mu2, 1, 0.01)
-check("Pe", Pe, 0, 0.01)
-check("Ph ", Ph , 0, 0.01)
+check("A", A, 1.245741227364988e-014, 2)
+check("B1", B1, -9.855007421888915e-014, 2)
+check("B2", B2, 3.052725906923963e-014, 2)
+check("Sig", Sig, 0, 2)
+check("E", E, 4.235125240802008e-021, 3)
+check("H1", H1, -7.842365727004682e-008, 2)
+check("H2", H2, 2.429282089958189e-008, 2)
+check("Je", Je, 0, 2)
+check("Js", Js, 0, 2)
+check("Mu1", Mu1, 1, 2)
+check("Mu2", Mu2, 1, 2)
+check("Pe", Pe, 0, 2)
+check("Ph ", Ph , 0, 2)
 
 assert(SUCCESS)
 write("SUCCESS\n")
--- cfemm/femmcli/test/femmcli_hpproc.lua
+++ cfemm/femmcli/test/femmcli_hpproc.lua
@@ -6,15 +6,18 @@ showconsole()
 
 SUCCESS=1
 
+-- check variable <name>,
+-- compare <value> against <expected> value
+-- if the relative difference is greater than the margin (in percent), complain and set global variable SUCCESS to false
 function check(name, value, expected, margin)
-	diff=value - expected
+	diff=100*(value - expected) / expected
 	if abs(diff) > margin then
 		SUCCESS=0
 		result="[FAILED] "
 	else
 		result="[  ok  ] "
 	end
-	print(result .. name .. ": " .. value .. " (expected: " .. expected .. ", diff: " .. diff .. ", margin: " .. margin .. ")")
+	print(result .. name .. ": " .. value .. " (expected: " .. expected .. ", diff: " .. diff .. "%, margin: " .. margin .. "%)")
 	--print("check(\""..name.."\", "..name..", "..value..", "..margin..")")
 end
 
@@ -30,23 +33,23 @@ T,Fx,Fy,Gx,Gy,kx,ky= ho_getpointvalues(1.1,1.1)
 -- check result against FEMM42 output:
 -- FIXME: error margin needs sane values
 print("Checks against femm42 output:")
-check("T", T, 304.8641290114103, 0.01)
-check("Fx", Fx, 0.2199070927061962, 0.01)
-check("Fy", Fy, 0.1428113935654898, 0.01)
-check("Gx", Gx, 8.313999477015031, 0.01)
-check("Gy", Gy, 5.399252187839117, 0.01)
-check("kx", kx, 0.02645021728882154, 0.01)
-check("ky", ky, 0.02645021728882154, 0.01)
+check("T", T, 304.8641290114103, 2)
+check("Fx", Fx, 0.2199070927061962, 2)
+check("Fy", Fy, 0.1428113935654898, 2)
+check("Gx", Gx, 8.313999477015031, 2)
+check("Gy", Gy, 5.399252187839117, 2)
+check("kx", kx, 0.02645021728882154, 2)
+check("ky", ky, 0.02645021728882154, 2)
 
 -- xfemm output before lua integration:
 print("Checks against known pre-lua xfemm output:")
-check("T", T, 304.864129, 0.0001)
-check("Fx", Fx, 0.219907, 0.0001)
-check("Fy", Fy, 0.142811, 0.0001)
-check("Gx", Gx, 8.313999, 0.0001)
-check("Gy", Gy, 5.399252, 0.0001)
-check("kx", kx, 0.026450, 0.0001)
-check("ky", ky, 0.026450, 0.0001)
+check("T", T, 304.864129, 0.1)
+check("Fx", Fx, 0.219907, 0.1)
+check("Fy", Fy, 0.142811, 0.1)
+check("Gx", Gx, 8.313999, 0.1)
+check("Gy", Gy, 5.399252, 0.1)
+check("kx", kx, 0.026450, 0.1)
+check("ky", ky, 0.026450, 0.1)
 
 assert(SUCCESS)
 write("SUCCESS\n")
