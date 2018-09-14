aca1c16fc8f628c0d272dfa266a51243c2898dec
Richard Crozier <a@b.c>
2018-09-14 15:31:25 +0100

    release.sh: added options related to mex build, and added support for cross build of mex files

--- release.sh
+++ release.sh
@@ -20,17 +20,25 @@ run_tests=false
 copy_win_libs=true
 skip_mex=false
 make_zip=false
+matlab_cmd="matlab"
+matlab_extra_cmds=""
+make_win_mex=false
+matlab_win_lib_dir=""
 
-usage="$(basename "$0") [-h] [-v <version>] [-t] [-m] [-z]  -- creates xfemm release
+usage="$(basename "$0") [-h] [-v <version>] [-t] [-m] [-z] [-c] [-l <matlab_win_lib_dir>] [-e <matlab_extra_cmds>] -- creates xfemm release
 
 where:
     -h  show this help text
     -v  set the release version string (default: $version)
     -t  run tests
     -m  skip building mex files (requires matlab)
-    -z  create zip file"
+    -z  create zip file
+    -c  command to invoke matlab (default: matlab)
+    -w  attempt to cross-build matlab mex files (requires -l option)
+    -l  matlab windows libraries directory path
+    -e  additional command(s) to be run by matlab before building the mex files"
 
-while getopts "h?v:twmz" opt; do
+while getopts "h?v:twmzc:wl:e:" opt; do
     case "$opt" in
     h|\?)
         echo "$usage"
@@ -48,93 +56,127 @@ while getopts "h?v:twmz" opt; do
     z)  make_zip=true
         echo "make_zip: $make_zip"
         ;;
+    c)  matlab_cmd=$OPTARG
+        echo "matlab_cmd: $matlab_cmd"
+        ;;
+    w)  make_win_mex=true
+        echo "make_win_mex: $make_win_mex"
+        ;;
+    l)  matlab_win_lib_dir=$OPTARG
+        echo "matlab_win_lib_dir: $matlab_win_lib_dir"
+        ;;
+    e)  matlab_extra_cmds=$OPTARG
+        echo "matlab_extra_cmds: $matlab_extra_cmds"
+        ;;
     esac
 done
 
-echo "Working copy dir is: $working_copy_dir"
-mkdir $working_copy_dir/release
+echo "Working copy dir is: ${working_copy_dir}"
+
 
 # create release directory
+release_prefix=/tmp/xfemm_release
+mkdir -p ${release_prefix}
 
 # linux 64 bit
-#mkdir $working_copy_dir/release/xfemm_linux64
-LINUX_64_DIR="xfemm_linux64"
-LINUX_64_RELEASE_DIR="$working_copy_dir/release/$LINUX_64_DIR"
+#mkdir ${working_copy_dir}/release/xfemm_linux64
+linux_64_common_dir_name="xfemm_linux64"
+linux_64_release_dir="$release_prefix/${linux_64_common_dir_name}"
 # export from the working directory to the release directory
-hg archive $LINUX_64_RELEASE_DIR
+hg archive ${linux_64_release_dir}
 # remove file created by mercurial
-rm $LINUX_64_RELEASE_DIR/.hg_archival.txt
+rm ${linux_64_release_dir}/.hg_archival.txt
 # remove the release script
-rm $LINUX_64_RELEASE_DIR/release.sh
-rm $LINUX_64_RELEASE_DIR/test_release.sh
+rm ${linux_64_release_dir}/release.sh
+rm ${linux_64_release_dir}/test_release.sh
+# create version file for cmake to find
+echo "${version}" > "${linux_64_release_dir}/cfemm/VERSION"
+
 # create temp build directory
-rm -rf /tmp/$LINUX_64_DIR
-mkdir -p /tmp/$LINUX_64_DIR
+rm -rf /tmp/${linux_64_common_dir_name}
+mkdir -p /tmp/${linux_64_common_dir_name}
 # run cmake from temp dir and build
-cd /tmp/$LINUX_64_DIR
-cmake -DCMAKE_BUILD_TYPE=Release $LINUX_64_RELEASE_DIR/cfemm
+cd /tmp/${linux_64_common_dir_name}
+cmake -DCMAKE_BUILD_TYPE=Release ${linux_64_release_dir}/cfemm
 make
 
 if [ "$skip_mex" = false ]; then
 
-  if ! [ -x "$(command -v matlab)" ]; then
+  if ! [ -x "$(command -v ${matlab_cmd})" ]; then
     echo 'matlab is not installed, not building mex files using Matlab.' >&2
   else
     # buld the mex files using matlab
-    matlab -nodesktop -r "restoredefaultpath; cd('$LINUX_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', true, 'ForceMexRecompile', true); quit"
-    #matlab -nodesktop -r "restoredefaultpath; cd('$LINUX_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', false, 'ForceMexRecompile', true, 'CrossBuildW64', true); quit"
+    #${matlab_cmd} -nodesktop -r "restoredefaultpath; cd('${linux_64_release_dir}/mfemm'); mfemm_setup('Runtests', true, 'ForceMexRecompile', true); quit"
+
+    ${matlab_cmd} -nodesktop -r "restoredefaultpath; cd('${linux_64_release_dir}/mfemm'); ${matlab_extra_cmds}; mfemm_setup('Runtests', true, 'ForceMexRecompile', true); quit"
+
+    if [ "$make_win_mex" = true ]; then
+        ${matlab_cmd} -nodesktop -r "restoredefaultpath; cd('${linux_64_release_dir}/mfemm'); ${matlab_extra_cmds}; mfemm_setup('DoCrossBuildWin64', true, 'W64CrossBuildMexLibsDir', '${matlab_win_lib_dir}', 'Runtests', false, 'ForceMexRecompile', true); quit"
+    fi
+
+    #matlab -nodesktop -r "restoredefaultpath; cd('${linux_64_release_dir}/mfemm'); mfemm_setup('Runtests', false, 'ForceMexRecompile', true, 'CrossBuildW64', true); quit"
   fi
 else
   echo 'not building mex files using Matlab as -m option has been used.' >&2
 fi
 
 # tar up the result in the release directory
-cd $working_copy_dir/release
-tar cvzf xfemm_v${version}_linux64.tar.gz $LINUX_64_DIR/
+cd ${working_copy_dir}/release
+tar cvzf xfemm_v${version}_linux64.tar.gz ${linux_64_common_dir_name}/
 
 
 # repeat the above, but cross-compiling for windows, requires MXE: http://mxe.cc/
 #
 
 # win 32 -- i686-pc-mingw32
-#svn export $working_copy_dir $working_copy_dir/release/xfemm_mingw_win32 --native-eol CRLF
-#rm $working_copy_dir/release/xfemm_mingw_win32/release.sh
-WIN_64_DIR="xfemm_mingw_win64"
-WIN_64_RELEASE_DIR="$working_copy_dir/release/$WIN_64_DIR"
+#svn export ${working_copy_dir} ${working_copy_dir}/release/xfemm_mingw_win32 --native-eol CRLF
+#rm ${working_copy_dir}/release/xfemm_mingw_win32/release.sh
+win_64_common_dir_name="xfemm_mingw_win64"
+win_64_release_dir="${release_prefix}/${win_64_common_dir_name}"
 # win 64 -- x86_64-w64-mingw32 MXE target
-hg archive $WIN_64_RELEASE_DIR # TODO: windows line endings?
+hg archive ${win_64_release_dir} # TODO: windows line endings?
 # remove file created by hg
-rm $WIN_64_RELEASE_DIR/.hg_archival.txt
+rm ${win_64_release_dir}/.hg_archival.txt
 # remove release scripts
-rm $WIN_64_RELEASE_DIR/release.sh
-rm $WIN_64_RELEASE_DIR/test_release.sh
+rm ${win_64_release_dir}/release.sh
+rm ${win_64_release_dir}/test_release.sh
+# create version file for cmake to find
+echo "${version}" > "${win_64_release_dir}/cfemm/VERSION"
 # create temp build directory
-rm -rf /tmp/$WIN_64_DIR
-mkdir -p /tmp/$WIN_64_DIR
+rm -rf /tmp/${win_64_common_dir_name}
+mkdir -p /tmp/${win_64_common_dir_name}
 # run cmake from temp dir and build cfemm
-cd /tmp/$WIN_64_DIR
+cd /tmp/${win_64_common_dir_name}
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DEXTRA_CMAKE_CXX_FLAGS="-static -static-libgcc -static-libstdc++" \
       -DEXTRA_CMAKE_EXE_LINKER_FLAGS="-static  -static-libgcc -static-libstdc++" \
-      -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/x86_64-w64-mingw32.static/share/cmake/mxe-conf.cmake $WIN_64_RELEASE_DIR/cfemm
+      -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/x86_64-w64-mingw32.static/share/cmake/mxe-conf.cmake ${win_64_release_dir}/cfemm
 make
 
 # copy over the windows dlls, and delete from dist
-#cp $working_copy_dir/release/xfemm_mingw_win64/windeps/* $working_copy_dir/release/xfemm_mingw_win64/cfemm/lib/
-#cp $working_copy_dir/release/xfemm_mingw_win64/windeps/* $working_copy_dir/release/xfemm_mingw_win64/cfemm/bin/
-#rm -r $working_copy_dir/release/xfemm_mingw_win64/windeps/
+#cp ${working_copy_dir}/release/xfemm_mingw_win64/windeps/* ${working_copy_dir}/release/xfemm_mingw_win64/cfemm/lib/
+#cp ${working_copy_dir}/release/xfemm_mingw_win64/windeps/* ${working_copy_dir}/release/xfemm_mingw_win64/cfemm/bin/
+#rm -r ${working_copy_dir}/release/xfemm_mingw_win64/windeps/
 
-if ! [ -x "$(command -v matlab)" ]; then
-  echo 'matlab is not installed, not building mex files using Matlab.' >&2
+if [ "$skip_mex" = false ]; then
+
+  if ! [ -x "$(command -v ${matlab_cmd})" ]; then
+    echo 'matlab is not installed, not building mex files using Matlab.' >&2
+  else
+    # buld the mex files using matlab
+    ${matlab_cmd} -nodesktop -r "restoredefaultpath; cd('${win_64_release_dir}/mfemm'); ${matlab_extra_cmds}; mfemm_setup('Runtests', true, 'ForceMexRecompile', true); quit"
+
+    if [ "$make_win_mex" = true ]; then
+        ${matlab_cmd} -nodesktop -r "restoredefaultpath; cd('${win_64_release_dir}/mfemm'); ${matlab_extra_cmds}; mfemm_setup('DoCrossBuildWin64', true, 'W64CrossBuildMexLibsDir', '${matlab_win_lib_dir}', 'Runtests', false, 'ForceMexRecompile', true); quit"
+    fi
+  fi
 else
-  # buld the mex files using matlab
-  matlab -nodesktop -r "restoredefaultpath; cd('$WIN_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', true, 'ForceMexRecompile', true); quit"
-  #matlab -nodesktop -r "restoredefaultpath; cd('$WIN_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', false, 'ForceMexRecompile', true, 'CrossBuildW64', true); quit"
+  echo 'not building mex files using Matlab as -m option has been used.' >&2
 fi
 
 # zip up the result in the release directory
-#cd $working_copy_dir/release
-#zip -r xfemm_v${version}_mingw_win64.zip $WIN_64_DIR/
+#cd ${working_copy_dir}/release
+#zip -r xfemm_v${version}_mingw_win64.zip ${win_64_common_dir_name}/
 
 if [ "$run_tests" = true ]; then
   # test
