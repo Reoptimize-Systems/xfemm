0a6571ae6f4a6808387616e08e4fbcd78588f45e
Richard Crozier <richard.crozier@power-enable.co.uk>
2018-10-09 10:06:30 +0100

    +mfemmdeps/mmake_check_cross.m: add help text

--- mfemm/+mfemmdeps/mmake_check_cross.m
+++ mfemm/+mfemmdeps/mmake_check_cross.m
@@ -1,4 +1,32 @@
 function vars = mmake_check_cross (winmexlibdir, vars)
+% process cross-compiling mmake variables and check the cross libs directory
+%
+% Syntax
+%
+% vars = mmake_check_cross (winmexlibdir, vars)
+%
+% Decription
+%
+% mmake_check_cross is a function used by the mfemm mex function build
+% system when cross-compiling the mex files on a Linux host for a win64
+% target. It checks the specified location of the windows mex libraries and
+% adds apporpriate entries to the mmake vars structure.
+%
+% Inputs
+%
+%  vars - structure containing at least the field 'MEXFLAGS'. This will
+%    have a character vector appended adding the windows matlab libraries
+%    directory to the compiler search path using the -L switch.
+%
+% Outputs
+%
+%  vars - structure containing at least the fields 'MEXFLAGS' and COMPILER.
+%    The MEXFLAGS will have a character vector appended adding the windows
+%    matlab libraries directory to the compiler search path using the -L
+%    switch. The COMPILER field will be a character vector containing the
+%    full path to the cross compiler executable.
+%
+%
 
     assert ( ~isempty (winmexlibdir), ...
              sprintf ( ['W64CrossBuild is true, but W64CrossBuildMexLibsDir is empty.\n', ...
