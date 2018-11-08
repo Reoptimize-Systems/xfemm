7300574854e9413cf12f3b2896baec4a1d55d796
Richard Crozier <a@b.c>
2018-11-08 12:51:43 +0000

    Test_xfemm_air_gap_boundary.m: expand to do full comparisons of all components

--- mfemm/testing/Test_xfemm_air_gap_boundary.m
+++ mfemm/testing/Test_xfemm_air_gap_boundary.m
@@ -12,9 +12,35 @@ fp = loadfemmfile (problemfile);
 
 sol = fpproc (firstansfilename);
 
-tq(1) = sol.gapintegral ('AGE', 0);
+tq = sol.gapintegral ('AGE', 0);
 
-angles = linspace (0, 90, 50);
+opendocument (femfilename);
+opendocument (firstansfilename);
+
+tq_femm_pp = mo_gapintegral ('AGE', 0);
+
+mi_close ();
+mo_close ();
+
+newfemfilename = [tempname(), '.fem'];
+newansfilename = strrep (newfemfilename, '.fem', '.ans');
+copyfile (femfilename, newfemfilename);
+opendocument (newfemfilename);
+
+mi_analyse (1);
+
+mi_loadsolution ();
+
+tq_femm_all = mo_gapintegral ('AGE', 0);
+
+sol = fpproc (newansfilename);
+    
+tq_femm_sol_fpproc_pp = sol.gapintegral ('AGE', 0);
+
+delete (newfemfilename);
+delete (newansfilename);
+    
+angles = linspace (0, 90, 10);
 
 for ind = 2:numel (angles)
    
@@ -23,17 +49,43 @@ for ind = 2:numel (angles)
     
     fp.BoundaryProps(3).InnerAngle = angles(ind);
     
-    [ansfilename, femfilename] = analyse_mfemm(fp, 'Quiet', false, 'KeepMesh', true);
+    [ansfilename, femfilename] = analyse_mfemm(fp, 'Quiet', false, 'KeepMesh', false);
     
     sol = fpproc (ansfilename);
     
-%     opendocument (ansfilename); pause (1);
-    
     tq(ind) = sol.gapintegral ('AGE', 0);
     
-    delete (ansfilename);
+    opendocument (femfilename);
+    opendocument (ansfilename);
+    
+    tq_femm_pp(ind) = mo_gapintegral ('AGE', 0);
+    
+    mi_close ();
+    mo_close ();
+    
+    newfemfilename = [tempname(), '.fem'];
+    newansfilename = strrep (newfemfilename, '.fem', '.ans');
+    copyfile (femfilename, newfemfilename);
+    opendocument (newfemfilename);
+    
+    mi_analyse (1);
+    
+    mi_loadsolution ();
+    
+    tq_femm_all(ind) = mo_gapintegral ('AGE', 0);
+    
+    sol = fpproc (newansfilename);
+    
+    tq_femm_sol_fpproc_pp(ind) = sol.gapintegral ('AGE', 0);
+    
     delete (femfilename);
+    delete (ansfilename);
+    delete (newfemfilename);
+    delete (newansfilename);
     
 end
 
-plot (angles, tq);
+plot (angles, [tq(:), tq_femm_pp(:), tq_femm_all(:), tq_femm_sol_fpproc_pp(:)]);
+
+legend ('xfemm solver, xfemm pp', 'xfemm solver, FEMM PP', 'FEMM Solver, FEMM PP', 'FEMM Solver, xfemm pp');
+
