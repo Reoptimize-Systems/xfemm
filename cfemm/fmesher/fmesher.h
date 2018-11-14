96f120f0a3e041e4ed4b71aace0e11c7621c73d6
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-11-14 13:23:37 +0000

    fmesher.h: define SNPRINTF macro

--- cfemm/fmesher/fmesher.h
+++ cfemm/fmesher/fmesher.h
@@ -55,6 +55,17 @@
 #define BoundingBoxFraction 100.0
 #endif
 
+#ifndef _MSC_VER
+#define _strnicmp strncasecmp
+#ifndef SNPRINTF
+  #define SNPRINTF std::snprintf
+#endif
+#else
+#ifndef SNPRINTF
+  #define SNPRINTF _snprintf
+#endif
+#endif
+
 namespace fmesher
 {
 
